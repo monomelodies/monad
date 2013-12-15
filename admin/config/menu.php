709726ab5578f305173db2094617da9722e7ae46
Marijn Ophorst <marijn@sensimedia.nl>
2013-12-15 10:57:31 +0100

    be more concise

--- admin/config/menu.php
+++ admin/config/menu.php
@@ -3,8 +3,7 @@
 namespace monad\admin;
 
 $menu->using(__NAMESPACE__, function() use($menu) {
-    $group = $menu->group('content');
-    $group->add('page');
-    $group->add('menu');
+    $menu->group('content')->add('page')
+                           ->add('menu');
 });
 
