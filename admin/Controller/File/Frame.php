101146122bd56776b7a7d119ab057df209ad5977
Marijn Ophorst <marijn@sensimedia.nl>
2013-12-15 11:58:36 +0100

    modernize

--- admin/Controller/File/Frame.php
+++ admin/Controller/File/Frame.php
@@ -1,22 +1,20 @@
 <?php
 
 namespace monad\admin;
+use monad\core;
 
-class Frame_File_Controller extends Controller
+class Frame_File_Controller extends core\Controller
 {
     protected function get(array $args)
     {
-        $this->template = false;
+        $this->template = $this->view('monolyth\template/page');
         extract($args);
         $media = null;
         if ($id and !($media = $this->medias->find($id))) {
             throw new HTTP404_Exception;
         }
         $this->input->prepare('file');
-        return $this->view(
-            ['template/file/frame', 'monolyth\template/page'],
-            compact('media')
-        );
+        return $this->view('template/file/frame', compact('media'));
     }
 }
 
