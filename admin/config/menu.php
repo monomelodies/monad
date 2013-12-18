2f16363294751a048071a45d9403eef90fe403b9
Marijn Ophorst <marijn@sensimedia.nl>
2013-12-18 13:17:27 +0100

    wrap

--- admin/config/menu.php
+++ admin/config/menu.php
@@ -3,7 +3,9 @@
 namespace monad\admin;
 
 $menu->using(__NAMESPACE__, function() use($menu) {
-    $menu->group('content')->add('page')
-                           ->add('menu');
+    if (in_array('Administrator', $menu->user->groups())) {
+        $menu->group('content')->add('page')
+                               ->add('menu');
+    }
 });
 
