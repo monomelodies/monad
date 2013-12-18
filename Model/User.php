3d12d5aa06134d5939b83e7697f0c4d95aa9da0f
Marijn Ophorst <marijn@sensimedia.nl>
2013-12-18 13:16:24 +0100

    use the group check

--- Model/User.php
+++ Model/User.php
@@ -7,13 +7,7 @@ class User_Model extends monolyth\User_Model
 {
     public function loggedIn()
     {
-        return parent::loggedIn() && $this->isAdmin('monad');
-    }
-
-    public function isAdmin($name)
-    {
-        $acl = $this->acl;
-        return $acl->using($name)->can($acl::READ);
+        return parent::loggedIn() && $this->inGroup('Monad');
     }
 }
 
