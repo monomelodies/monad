8c5b955e8c527ef0d85a19ac44d1fdfec429b89c
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-06 08:12:14 +0100

    watfak

--- admin/output/html/slice/inline/tabular.php
+++ admin/output/html/slice/inline/tabular.php
@@ -21,13 +21,17 @@ if (method_exists($model, 'inlineLinks')) {
             <tr>
                 <th colspan="<?=$items[0] instanceof Sortable ? 4 : 3?>" class="actions"><?=$text('actions')?></th>
 <?php foreach ($items[0] as $field => $dummy) { ?>
-                <th><th><?=$text([$generate($items[0], "./$field"), "$namespace\\column/$field", __NAMESPACE__."\\column/$field"])?></th></th>
+                <th><?=$text([$generate($items[0], "./$field"), "$namespace\\column/$field", __NAMESPACE__."\\column/$field"])?></th>
 <?php } ?>
+<?php if ($items[0] instanceof Uncreateable_Model) { ?>
+                <th></th>
+<?php } else { ?>
                 <th class="action"><a href="<?=$scaffold(
                     'create',
                     $database,
                     $items[0]
                 ).$params?>" class="icon create" title="<?=htmlentities($text('create'))?>"><?=$text('create')?></a></th>
+<?php } ?>
             </tr>
         </thead>
         <tbody>
@@ -38,10 +42,18 @@ if (method_exists($model, 'inlineLinks')) {
 <?php   } ?>
                 <td class="action"><a href="<?=$scaffold('update', $database, $item).$params
                     ?>" class="icon update" title="<?=htmlentities($text('update'))?>"><?=$text('update')?></a></td>
+<?php   if (!($item instanceof Uncreateable_Model)) { ?>
                 <td class="action"><a href="<?=$scaffold('copy', $database, $item).$params
                     ?>" class="icon copy" title="<?=htmlentities($text('copy'))?>"><?=$text('copy')?></a></td>
+<?php   } else { ?>
+                <td class="action"></td>
+<?php   } ?>
+<?php   if (method_exists($item, 'delete')) { ?>
                 <td class="action"><a href="<?=$scaffold('delete', $database, $item).$params
                     ?>" class="icon delete" title="<?=htmlentities($text('delete'))?>"><?=$text('delete')?></a></td>
+<?php   } else { ?>
+                <td class="action"></td>
+<?php   } ?>
 <?php   foreach ($item as $field => $value) { ?>
                 <td<?=is_numeric($value) ? ' class="numeric"' : ''?>><?=$value?></td>
 <?php   } ?>
