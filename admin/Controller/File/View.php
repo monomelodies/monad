beef47afd8aa16de5a813546f7ac802f0606a309
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-06 08:30:20 +0100

    not perfect, but modernize somewhat

--- admin/Controller/File/View.php
+++ admin/Controller/File/View.php
@@ -4,11 +4,13 @@ namespace monad\admin;
 
 class View_File_Controller extends Controller
 {
+    use Media_Access;
+
     protected function get(array $args)
     {
         $this->template = false;
         extract($args);
-        if (!($media = $this->medias->find($id))) {
+        if (!($media = self::medias()->find($id))) {
             throw new HTTP404_Exception;
         }
         return $this->view('template/file/view', compact('media'));
