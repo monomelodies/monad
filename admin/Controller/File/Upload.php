08412a69efa1809e111ba0a1bb7cc631900a200e
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-09 20:06:21 +0100

    modernize, at least v1

--- admin/Controller/File/Upload.php
+++ admin/Controller/File/Upload.php
@@ -4,21 +4,22 @@ namespace monad\admin;
 use monolyth\HTTP301_Exception;
 use ErrorException;
 use monad\core\Controller;
+use monolyth\Media_Model;
 
 class Upload_File_Controller extends Controller
 {
     protected function post(array $args)
     {
+        $media = new Media_Model;
         if (!isset($_FILES['file'])) {
-            throw new HTTP301_Exception($this->http->getRedir());
+            throw new HTTP301_Exception(self::http()->getRedir());
         }
-        if ($error = $this->media->create(
+        if ($error = $media->create(
             $_FILES['file'],
             isset($_GET['id']) ? $_GET['id'] : null
         )) {
             var_dump($error);
         }
-        $media = $this->media;
         $this->template = null;
         return $this->view('template/file/done', compact('media'));
     }
