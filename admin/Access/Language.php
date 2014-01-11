ce49252881d1d4230906a737f06df15d4fcc7795
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-11 19:31:47 +0100

    this is much better handled in the trait itself

--- admin/Access/Language.php
+++ admin/Access/Language.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace monad\admin;
+use monolyth;
 
 trait Language_Access
 {
+    use monolyth\Language_Access {
+        monolyth\Language_Access::language as projectlanguage;
+    }
+
     public static function language()
     {
         static $language;
