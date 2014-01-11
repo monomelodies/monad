760911cb089b3cdd942ed54623ca9bb81794a4a5
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-11 19:24:54 +0100

    fix typo and use inGroup method

--- admin/config/menu.php
+++ admin/config/menu.php
@@ -3,7 +3,7 @@
 namespace monad\admin;
 
 $menu->using(__NAMESPACE__, function() use($menu) {
-    if (in_array('Administrator', $menu->user()->groups())) {
+    if ($menu->user()->inGroup('Administrators')) {
         $menu->group('content')->add('page')
                                ->add('menu');
     }
