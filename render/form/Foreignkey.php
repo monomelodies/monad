42eee14c001038d2fa840faaeef724e7bad7a225
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-13 20:13:08 +0100

    different working of the foreignKey element

--- render/form/Foreignkey.php
+++ render/form/Foreignkey.php
@@ -3,34 +3,59 @@
 namespace monad\render\form;
 use monolyth\render\form\Text;
 use monolyth\adapter\sql\NoResults_Exception;
+use Adapter_Access;
 
 class Foreignkey extends Text
 {
+    use Adapter_Access;
+
     public $initial;
-    protected $type = 'foreignkey',
-              $renderOptions = ['id', 'name'],
-              $data = null;
+    protected $type = 'foreignkey';
+    protected $renderOptions = ['id', 'name'];
+    protected $data = null;
+    private $settings;
+    private $internalId;
+    private $originalInternalId;
 
-    public function prepare($name, callable $fn, array $d, array $o = [])
+    public function prepare($name, array $settings, array $options = [])
     {
-        parent::prepare($name, $o);
-        $this->data = $d;
-        try {
-            $this->initial = $fn();
-        } catch (NoResults_Exception $e) {
-            $this->initial = null;
+        parent::prepare($name, $options);
+        $this->settings = $settings;
+    }
+
+    public function __set($name, $value)
+    {
+        if ($name != 'value') {
+            return null;
         }
-    }
-
-    public function data()
-    {
-        return $this->data;
-    }
-
-    public function filter($elname)
-    {
-        $this->setOption('data-filter', $elname);
-        return $this;
+        if (!(isset($this->options['required'])
+            && $this->options['required']
+        )) {
+            if ((is_array($value) && !$value)
+                || (!is_array($value) && !strlen(trim($value)))
+            ) {
+                $value = null;
+            }
+        }
+        $id = null;
+        if ($value) {
+            $id = $value;
+            try {
+                $value = self::adapter()->get(
+                    $this->settings['table'],
+                    $this->settings['field'],
+                    compact('id')
+                );
+            } catch (NoResults_Exception $e) {
+                $value = null;
+            }
+        }
+        $this->original = $this->value;
+        $this->originalInternalId = $this->internalId;
+        $this->value = $value;
+        $this->internalId = $id;
+        unset($this->options[$name]);
+        return $value;
     }
 }
 
