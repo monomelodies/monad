0be8026c70b9f9b89b7f0e253031f8ab8ed0bb10
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-17 17:57:57 +0100

    add hidden item

--- admin/config/menu.php
+++ admin/config/menu.php
@@ -5,7 +5,8 @@ namespace monad\admin;
 $menu->using(__NAMESPACE__, function() use($menu) {
     $menu->requires('Administrators', function() use($menu) {
         $menu->group('content')->add('page')
-                               ->add('menu');
+                               ->add('menu')
+                               ->hidden('item_menu');
     });
 });
 
