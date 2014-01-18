8ad332347807c2ec1fd3e05ef98342b40772a00f
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-18 11:25:05 +0100

    use singleton

--- core/Finder.php
+++ core/Finder.php
@@ -4,10 +4,12 @@ namespace monad\core;
 use monolyth;
 use monolyth\adapter;
 use Adapter_Access;
+use monolyth\core\Singleton;
 
 abstract class Finder implements monolyth\Finder
 {
     use Adapter_Access;
+    use Singleton;
 
     public $view = 'monad\admin\slice/table';
     public $stripTags = true;
