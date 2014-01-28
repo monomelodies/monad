e87dc43649dc2b2113560524e4d219db6b29e39d
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-28 14:08:52 +0100

    this needed some modernization

--- admin/Controller/File/Frame.php
+++ admin/Controller/File/Frame.php
@@ -2,19 +2,28 @@
 
 namespace monad\admin;
 use monad\core;
+use monolyth\render\form\File;
+use monolyth\render\Translate_Parser;
+use ErrorException;
 
 class Frame_File_Controller extends core\Controller
 {
     protected function get(array $args)
     {
         $this->template = $this->view('monolyth\template/page');
+        $this->translate = new Translate_Parser;
         extract($args);
         $media = null;
-        if ($id and !($media = $this->medias->find($id))) {
+        if ($id and !($media = Media_Finder::instance()->find($id))) {
             throw new HTTP404_Exception;
         }
-        $this->input->prepare('file');
-        return $this->view('template/file/frame', compact('media'));
+        $input = new File;
+        $input->prepare('file');
+        $parse = true;
+        return $this->view(
+            'template/file/frame',
+            compact('media', 'input', 'parse')
+        );
     }
 }
 
