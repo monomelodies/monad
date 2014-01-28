b6d76935e6bf081905d97e529ba37ab177ee2bb7
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-28 14:11:01 +0100

    fix typo

--- render/form/output/html/slice/media.php
+++ render/form/output/html/slice/media.php
@@ -1,4 +1,4 @@
-<?php namespace monad\rendoer\form ?>
+<?php namespace monad\render\form ?>
 <iframe id="<?=$o->getOption('id')?>" class="monad_file <?=$o->getOption('class')?>" data-id="<?=$o->value?>" src="<?=$url(
     'monad/admin/frame_file',
     ['id' => $o->value]
