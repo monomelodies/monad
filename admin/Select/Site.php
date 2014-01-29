3384a88680011fdc5f41766654ab186ba7c9ce4c
Marijn Ophorst <marijn@sensimedia.nl>
2014-01-29 15:57:59 +0100

    project

--- admin/Select/Site.php
+++ admin/Select/Site.php
@@ -2,9 +2,8 @@
 
 namespace monad\admin;
 use monolyth\render\form\Select;
-use monolyth\Project_Access;
 
-class Site_Select extends Select implements Project_Access
+class Site_Select extends Select
 {
 }
 
