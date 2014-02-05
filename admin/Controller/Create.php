249303b0c77b51399d603c6b5f7979abecafeaed
Marijn Ophorst <marijn@sensimedia.nl>
2014-02-05 11:09:19 +0100

    this smells like code duplication, but at least this works now...

--- admin/Controller/Create.php
+++ admin/Controller/Create.php
@@ -2,11 +2,60 @@
 
 namespace monad\admin;
 use monolyth\HTTP301_Exception;
+use monolyth\adapter\sql\Adapter;
+use Adapter_Access;
+use monolyth\HTTP404_Exception;
 
 class Create_Controller extends Update_Controller
 {
+    use Adapter_Access;
+
     public function get(array $args)
     {
+        extract($args);
+        if (!isset($database)) {
+            $databases = [];
+            foreach (self::adapters() as $name => $db) {
+                if ($db instanceof Closure) {
+                    $db = $db();
+                }
+                if ($name == '_current' || !($db instanceof Adapter)) {
+                    continue;
+                }
+                // Check if adapter actually supplies required tables.
+                $requireds = 0;
+                foreach ($this->model->requires as $table) {
+                    try {
+                        $db->field($table, 1, []);
+                        $requireds++;
+                    } catch (NoResults_Exception $e) {
+                        $requireds++;
+                    } catch (Exception $e) {
+                    }
+                }
+                if ($requireds == count($this->model->requires)) {
+                    $databases[] = $name;
+                }
+            }
+            switch (count($databases)) {
+                case 0:
+                    throw new HTTP404_Exception;
+                case 1:
+                    throw new HTTP301_Exception($this->url(
+                        'monad/admin/create',
+                        [
+                            'package' => $args['package'],
+                            'target' => $args['target'],
+                            'database' => $databases[0],
+                        ]
+                    ));
+                default:
+                    return $this->view(
+                        'page/databases',
+                        $args + compact('databases')
+                    );
+            }
+        }
         unset($args['language']);
         $inlines = $this->model->inlines();
         if ($inlines) {
