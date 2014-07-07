c1f4e6563c85839dbbc3ff1a4e8558c283e90a67
Marijn Ophorst <marijn@sensimedia.nl>
2014-07-07 12:27:47 +0200

    add body-specific route so we can use it in ng-include (we also need the json variant for stuff like setting the title)

--- api/config/routing.php
+++ api/config/routing.php
@@ -4,6 +4,10 @@ namespace monad\api;
 
 return function($m) use($project) {
     $m->connect('/monad/page/(%s:language)/(%s:slug)/', 'monad\api\Page');
+    $m->connect(
+        '/monad/page/(%s:language)/(%s:slug)/body/',
+        'monad\api\Body_Page'
+    );
     $m->connect('/monad/section/(%s:language)/(%s:slug)/', 'monad\api\Section');
     return $m;
 };
