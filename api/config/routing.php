4e4c5a9fcacd7c45dbc10f504d558b7d5ce7e21c
Marijn Ophorst <marijn@sensimedia.nl>
2014-09-09 10:25:05 +0200

    hm, urls get fucked up since it thinks it is in the admin section... crap...

--- api/config/routing.php
+++ api/config/routing.php
@@ -3,12 +3,12 @@
 namespace monad\api;
 
 return function($m) use($project) {
-    $m->connect('/monad/page/(%s:language)/(%s:slug)/', 'monad\api\Page');
+    $m->connect('/page/(%s:language)/(%s:slug)/', 'monad\api\Page');
     $m->connect(
-        '/monad/page/(%s:language)/(%s:slug)/body/',
+        '/page/(%s:language)/(%s:slug)/body/',
         'monad\api\Body_Page'
     );
-    $m->connect('/monad/section/(%s:language)/(%s:slug)/', 'monad\api\Section');
+    $m->connect('/section/(%s:language)/(%s:slug)/', 'monad\api\Section');
     return $m;
 };
 
