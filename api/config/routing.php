457a2ff2f4da2a596a9817328d3d5393ba99d59e
Marijn Ophorst <marijn@sensimedia.nl>
2014-10-09 09:56:50 +0200

    use better tilde-prefixed api routing

--- api/config/routing.php
+++ api/config/routing.php
@@ -3,12 +3,15 @@
 namespace monad\api;
 
 return function($m) use($project) {
-    $m->connect('/page/(%s:language)/(%s:slug)/', 'monad\api\Page');
+    $m->connect('/~/monad/page/(%s:language)/(%s:slug)/', 'monad\api\Page');
     $m->connect(
-        '/page/(%s:language)/(%s:slug)/body/',
+        '/~/monad/page/(%s:language)/(%s:slug)/body/',
         'monad\api\Body_Page'
     );
-    $m->connect('/section/(%s:language)/(%s:slug)/', 'monad\api\Section');
+    $m->connect(
+        '/~/monad/section/(%s:language)/(%s:slug)/',
+        'monad\api\Section'
+    );
     return $m;
 };
 
