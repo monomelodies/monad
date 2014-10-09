757243c89ca862c944f93adf1066f4b89a37054d
Marijn Ophorst <marijn@sensimedia.nl>
2014-10-09 15:56:58 +0200

    cleanup/fix

--- admin/template/body.php
+++ admin/template/body.php
@@ -1,29 +1,5 @@
-<?php
-
-namespace monad\admin;
-/*
-$Css->unshift(
-    'monolyth/output/css/reset.css',
-    'monolyth/output/css/html5.css',
-    'monolyth/output/js/formalize/css/formalize.css',
-    'monad/output/css/layout.css',
-    "monad/output/css/{$project['theme']}/{$project['theme']}.css",
-    'output/css/monad.css?'
-);
-$Script->unshift(
-    'monolyth/output/js/formalize/js/jquery.formalize.js',
-    'monolyth/output/js/jquery/ui.timepicker.js',
-    'monolyth/output/js/jquery/validator.js',
-    'monolyth/output/js/jquery/rangeinput.js',
-    'monolyth/output/js/jquery/html5.js',
-    'monolyth/output/js/monolyth.js',
-    'monolyth/output/js/text.js',
-    'monad/output/js/core.js'
-);
-*/
-
-?>
-<body>
+<?php namespace monad\admin ?>
+<body<?=$self instanceof login\Controller ? ' class="valign"' : ''?>>
     <script>window.Monad = window.Monad || {}; window.Monad.language = '<?=$language->current->code?>'</script>
     <header id="header">
         <nav id="modules"><ul>
@@ -76,9 +52,17 @@ $Script->unshift(
     </ul></footer>
     <script src="/monolyth/js/modernizr.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-    <script src="/ckeditor/ckeditor.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
-    <script src="/ckeditor/adapters/jquery.js"></script>
+    <script src="/js/monad/ckeditor/ckeditor.js"></script>
+    <script src="/js/monad/ckeditor/adapters/jquery.js"></script>
+    <script src="/js/monad/formalize/js/jquery.formalize.js"></script>
+    <script src="/js/monolyth/jquery/ui.timepicker.js"></script>
+    <script src="/js/monolyth/jquery/validator.js"></script>
+    <script src="/js/monolyth/jquery/rangeinput.js"></script>
+    <script src="/js/monolyth/jquery/html5.js"></script>
+    <script src="/js/monolyth/monolyth.js"></script>
+    <script src="/js/monolyth/text.js"></script>
+    <script src="/js/monad/core.js"></script>
     <script>
         var Monad = Monad || {};
         Monad.language = '<?=$language->current->code?>';
