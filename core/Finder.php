286811198b385b714360e0cafccbf2c276b890f9
Marijn Ophorst <marijn@sensimedia.nl>
2014-10-09 20:52:13 +0200

    this can be cleaned up again now

--- core/Finder.php
+++ core/Finder.php
@@ -8,33 +8,14 @@ use monolyth\core\Singleton;
 
 abstract class Finder implements monolyth\Finder
 {
-    use Adapter_Access, Singleton {
-        Adapter_Access::adapter as _adapter;
-        Singleton::instance as _instance;
-    }
+    use Adapter_Access;
+    use Singleton;
 
     public $view = 'monad\admin\table';
     public $stripTags = true;
     protected static $db;
 
     public abstract function all($s, $p, array $w = [], array $o = []);
-
-    public static function instance($db = null)
-    {
-        $object = self::_instance();
-        $object->using($db);
-        return $object;
-    }
-
-    public function using($db)
-    {
-        self::$db = $db;
-    }
-
-    public static function adapter($db = null)
-    {
-        return self::_adapter(isset($db) ? $db : self::$db);
-    }
 
     public function models($m, $s, $p, array $w = [], array $o = [])
     {
