6e5230edc4e6c38d23f15792de4211a4bcc87087
Marijn Ophorst <marijn@sensimedia.nl>
2014-10-10 11:10:56 +0200

    let these work in a saner fashion

--- admin/Controller/Style/Wysiwyg.php
+++ admin/Controller/Style/Wysiwyg.php
@@ -23,21 +23,7 @@ class Wysiwyg_Style_Controller extends core\Controller
          *   partias, except probably for the topmost one (foo.css in our
          *   example).
          */
-        // Grab resets.
-        /*
-        foreach (['reset', 'html5'] as $file) {
-            include "monad/admin/_sass/_$file.scss";
-        }
-        */
-        // Grab custom fonts.
-        try {
-            ob_start();
-            include 'template/fonts.php';
-            echo str_replace(['<style>', '</style>'], '', ob_get_clean());
-        } catch (ErrorException $e) {
-            ob_end_clean();
-        }
-        $try = [$database, $package, $target, $field];
+        $try = [$package, $target, $field];
         if (isset($override)) {
             $try[] = $override;
         }
@@ -45,26 +31,7 @@ class Wysiwyg_Style_Controller extends core\Controller
         while ($try) {
             $paths[] = array_shift($try);
             try {
-                ob_start();
-                include 'admin/public/'.implode('/', $paths).'.css';
-                $css = ob_get_clean();
-                $css = preg_replace_callback(
-                    '/@import url\((.*?)\);/i',
-                    function($match) {
-                        /**
-                         * @import statements in generated wysiwyg styles for
-                         * MonAd are assumed to be relative to the include path,
-                         * e.g. @import url(output/css/project.css);
-                         */
-                        try {
-                            return file_get_contents($match[1], true);
-                        } catch (ErrorException $e) {
-                            return '';
-                        }
-                    },
-                    $css
-                );
-                $css = preg_replace_callback(
+                echo preg_replace("@(^|\s)(html|body)@", '', preg_replace_callback(
                     "@url\((.*?)\)@i",
                     function($match) {
                         if (preg_match("@^https?://@", $match[1])
@@ -74,24 +41,11 @@ class Wysiwyg_Style_Controller extends core\Controller
                         }
                         return 'url(/css/'.$match[1].')';
                     },
-                    $css
-                );
-                $cmd = sprintf(
-                    "php %s %s/bin/variables css",
-                    sprintf("-d include_path='%s'", get_include_path()),
-                    realpath(__DIR__.'/../../../../monolyth')
-                );
-                $r = proc_open(
-                    $cmd,
-                    [['pipe', 'r'], ['pipe', 'w'], ['pipe', 'a']],
-                    $pipes
-                );
-                foreach (str_split($css, 1024) as $line) {
-                    fwrite($pipes[0], $line);
-                }
-                fclose($pipes[0]);
-                echo stream_get_contents($pipes[1]);
-                proc_close($r);
+                    file_get_contents(
+                        'admin/public/'.implode('/', $paths).'.css',
+                        true
+                    )
+                ));
             } catch (ErrorException $e) {
             }
         }
