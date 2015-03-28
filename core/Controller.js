b43a08c79dd42f9a683d1d8db7ccf780449596b4
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-28 15:23:44 +0100

    make this configurable

--- core/Controller.js
+++ core/Controller.js
@@ -4,6 +4,12 @@ class Controller
     constructor($rootScope)
     {
         this.title = 'Default generic administrator';
+        this.paths = {
+            root: '/monad/',
+            bootstrap: '/vendor/github/twbs/bootstrap@3.3.4',
+            monad: '/vendor/github/monomelodies/monad@nextgen/build',
+            theme: undefined
+        };
     }
 }
 
