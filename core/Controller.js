8a0f9e4f41680382c9c18f39fca5e5a115b3f745
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-28 15:36:57 +0100

    add and expose navigation, lose rootScope dependency

--- core/Controller.js
+++ core/Controller.js
@@ -1,7 +1,7 @@
 
 class Controller
 {
-    constructor($rootScope)
+    constructor()
     {
         this.title = 'Default generic administrator';
         this.paths = {
@@ -10,10 +10,20 @@ class Controller
             monad: '/vendor/github/monomelodies/monad@nextgen',
             theme: undefined
         };
+        this.navigation = {
+            main: [
+                {url: '/', title: 'Site'}
+            ]
+        };
+    }
+    addToNavigation(module, menu = 'main')
+    {
+        this.navigation[menu] = this.navigation[menu] || [];
+        module.exposes.map(function(option) {
+            this.navigation[menu].push(option);
+        });
     }
 }
-
-Controller.$inject = ['$rootScope'];
 
 export {Controller};
 
