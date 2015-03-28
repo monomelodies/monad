6e2060f3fd1bca63f39a382d12615037a49fceab
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-28 16:21:00 +0100

    add register method

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -40,7 +40,25 @@ function NormalizePost($http)
     }];
 }
 
-export { NormalizePost, Controller };
+function register(app, items)
+{
+    for (var name in items) {
+        ['Controller', 'Service'].map(function(type) {
+            app[type.toLowerCase()](name + '.' + type, items[name][type]);
+        });
+        app.config(['$stateProvider', function($stateProvider) {
+            $stateProvider.
+                state(name, {
+                    url: '/' + name.toLowerCase() + '/',
+                    controller: items[name].Controller,
+                    controllerAs: 'list',
+                    templateUrl: './' + name + '/view.html'
+                });
+        }]);
+    }
+}
+
+export {NormalizePost, Controller, register};
 
 /*
             /*
