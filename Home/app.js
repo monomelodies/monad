316f724081e9be35ca3515a6a83bc8367d3bb448
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-28 09:14:37 +0100

    put this here

--- Home/app.js
+++ Home/app.js
@@ -1,7 +1,18 @@
 
 "use strict";
 
-import { Controller } from './Controller';
+import {Monad} from '../Monad';
+import {Controller} from './Controller';
+
+Monad.config(['$stateProvider', function($stateProvider) {
+    $stateProvider.
+        state('home', {
+            url: '/',
+            controller: Controller,
+            controllerAs: 'home',
+            templateUrl: './Home/view.html'
+        });
+}]);
 
 export { Controller };
 
