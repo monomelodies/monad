42618d07104d427c79d99adb84de35bdf1414bbd
Marijn Ophorst <marijn@sensimedia.nl>
2015-03-28 07:37:52 +0100

    update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ System.config({
   },
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
+    "github:*": "vendor/github/*.js",
+    "npm:*": "vendor/npm/*.js"
   }
 });
 
@@ -27,6 +27,12 @@ System.config({
     },
     "github:angular-ui/ui-router@0.2.13": {
       "angular": "github:angular/bower-angular@1.3.15"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@4.7.16": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
