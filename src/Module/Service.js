0ba8b5ab87a7d55d5eaedea11ebd91e4ca1ceff9
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-03 15:52:31 +0200

    v1 of improving this

---
+++ src/Module/Service.js
@@ -0,0 +1,29 @@
+
+"use strict";
+
+export class Service {
+
+    constructor() {
+    }
+    /*
+    for (var name in items) {
+        ['Controller', 'Service'].map(function(type) {
+            if (type in items[name]) {
+                app[type.toLowerCase()](name + '.' + type, items[name][type]);
+            } else {
+            }
+        });
+        app.config(['$stateProvider', function($stateProvider) {
+            $stateProvider.
+                state(name, {
+                    url: '/' + name.toLowerCase() + '/',
+                    controller: items[name].Controller,
+                    controllerAs: 'list',
+                    templateUrl: './' + name + '/view.html'
+                });
+        }]);
+    }
+    */
+
+};
+
