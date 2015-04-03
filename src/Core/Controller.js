64c5288bc600fcd758f5100b0f251196522f5ba2
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-03 15:51:59 +0200

    let this work properly

---
+++ src/Core/Controller.js
@@ -0,0 +1,40 @@
+
+class Controller {
+
+    constructor(AuthenticationService, $location, ModuleService) {
+        this.Authentication = AuthenticationService;
+        this.modules = ModuleService;
+        this.title = 'Default generic administrator';
+        this.loginRequired = this.loginRequired || true;
+        this.paths = {
+            root: '/monad/',
+            bootstrap: '/vendor/github/twbs/bootstrap@3.3.4',
+            monad: '/vendor/github/monomelodies/monad@nextgen'
+        };
+        this.paths.theme = this.paths.monad + '/css/default.css';
+        this.navigation = {
+            main: [
+                {url: '/', title: 'Site'}
+            ]
+        };
+        this.Authentication.read().success(result => {
+            if (!this.Authentication.isAuthenticated() && this.loginRequired) {
+                $location.path('/login/');
+            }
+        });
+        this.customize();
+    }
+
+    /**
+     * This is for overriding in an extending root controller, so we don't
+     * have to remember to re-inject all dependencies which would suck
+     * pretty badly.
+     */
+    config() {
+    }
+};
+
+Controller.$inject = ['AuthenticationService', '$location', 'ModuleService'];
+
+export {Controller};
+
