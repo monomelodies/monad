6f5b51d78e98096b2d3aff007a1d5105631bc306
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-03 15:52:44 +0200

    just be a bootstrap

---
+++ src/Module/app.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+import {ListController} from './ListController';
+import {ItemController} from './ItemController';
+import {Service} from './Service';
+
+export {ListController};
+export {ItemController};
+export {Service};
+
