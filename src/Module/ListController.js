c286286619e9c82943d8a4918cfbc084299e6980
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-06 14:52:56 +0200

    generic working

--- src/Module/ListController.js
+++ src/Module/ListController.js
@@ -3,9 +3,19 @@
 
 class ListController {
 
-    constructor() {
+    constructor(module, Service, Model, $routeParams) {
+        this.module = module;
+        this.Service = Service;
+        this.Model = Model;
+        this.page($routeParams);
+    }
+
+    page(params) {
+        this.Service.list(params).success(items => this.items = items);
     }
 };
+
+ListController.$inject = ['module', 'Service', 'Model', '$routeParams'];
 
 export {ListController};
 
