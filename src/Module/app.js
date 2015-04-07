20ff0e6bd9a2c2dea37064517325ec7def77bdf7
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-07 11:14:39 +0200

    modernize

--- src/Module/app.js
+++ src/Module/app.js
@@ -5,7 +5,11 @@ import {ListController} from './ListController';
 import {ItemController} from './ItemController';
 import {Service} from './Service';
 
-export {ListController};
-export {ItemController};
-export {Service};
+let ngModule = 'monad.Module';
+angular.module(ngModule, [])
+    .controller('Module.ListController', ListController)
+    .controller('Module.ItemController', ItemController)
+    .service('Module.Service', Service);
+
+export default ngModule;
 
