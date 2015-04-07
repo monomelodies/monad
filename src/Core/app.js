e5b13fa32dd051337a2e5c7b05a01e81394c3e5d
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-07 11:13:47 +0200

    cleanup/fix...almost satisfied :D

--- src/Core/app.js
+++ src/Core/app.js
@@ -1,73 +1,14 @@
 
 import {normalizePost} from './normalizePost';
 import {Controller} from './Controller';
+import {default as Authentication} from '../Authentication/app';
+import {default as Home} from '../Home/app';
+import {default as Module} from '../Module/app';
 
-function Module(app, items)
-{
-    for (var name in items) {
-        ['Controller', 'Service'].map(function(type) {
-            app[type.toLowerCase()](name + '.' + type, items[name][type]);
-        });
-        app.config(['$stateProvider', function($stateProvider) {
-            $stateProvider.
-                state(name, {
-                    url: '/' + name.toLowerCase() + '/',
-                    controller: items[name].Controller,
-                    controllerAs: 'list',
-                    templateUrl: './' + name + '/view.html'
-                });
-        }]);
-    }
-}
+let ngModule = 'monad.Core';
+angular.module(ngModule, ['pascalprecht.translate', 'ngRoute', Authentication, Home, Module])
+    .run(['$http', normalizePost])
+    .controller('Core.Controller', Controller);
 
-export {normalizePost, Module};
-export {Controller};
-
-/*
-            /*
-        }).
-        state('login', {
-            url: '/login/',
-            controller: LoginController,
-            controllerAs: 'login',
-            templateUrl: 'assets/html/login/view.html'
-import {HomeController} from './home/Controller';
-import {LoginController} from './login/Controller';
-
-class Monad
-{
-    constructor() {
-        this._angular = angular.module('monad', [
-            'ngRoute',
-            'ngAnimate',
-            'ngSanitize',
-            'pascalprecht.translate'
-        ]);
-        var that = this;
-        this._modules = [];
-    }
-    get angular() {
-        return this._angular;
-    }
-    registerModule(module) {
-        this._modules.push(module);
-    }
-    get modules() {
-        return this._modules;
-    }
-}
-
-class Module
-{
-    constructor() {
-        this._provides = [];
-    }
-    get provides() {
-        return this._provides;
-    }
-}
-
-var monad = new Monad;
-export {monad as Monad, Module};
-*/
+export default ngModule;
 
