05edf0e62202e897cedb8b4a5d9862f0f7a04f0b
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-07 16:47:03 +0200

    renamed

--- src/Core/app.js
+++ src/Core/app.js
@@ -3,10 +3,10 @@ import {normalizePost} from './normalizePost';
 import {Controller} from './Controller';
 import {default as Authentication} from '../Authentication/app';
 import {default as Home} from '../Home/app';
-import {default as Module} from '../Module/app';
+import {default as Data} from '../Data/app';
 
 let ngModule = 'monad.Core';
-angular.module(ngModule, ['pascalprecht.translate', 'ngRoute', Authentication, Home, Module])
+angular.module(ngModule, ['pascalprecht.translate', 'ngRoute', Authentication, Home, Data])
     .run(['$http', normalizePost])
     .controller('Core.Controller', Controller);
 
