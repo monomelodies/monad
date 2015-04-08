9011c493eb105f1d35cccbc7928ec5d0f077177e
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-08 21:06:53 +0200

    add dragdrop dependency

--- src/Core/app.js
+++ src/Core/app.js
@@ -8,7 +8,7 @@ import {default as Home} from '../Home/app';
 import {default as Data} from '../Data/app';
 
 let ngModule = 'monad.Core';
-angular.module(ngModule, ['pascalprecht.translate', 'ngRoute', Authentication, Home, Data, 'ngCkeditor'])
+angular.module(ngModule, ['pascalprecht.translate', 'ngRoute', Authentication, Home, Data, 'ngCkeditor', 'ngDragDrop'])
     .run(['$http', normalizePost])
     .controller('Core.Controller', Controller);
 
