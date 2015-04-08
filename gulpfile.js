ef00575d0625a8e252a49558fd9f0ab8ccc43b1a
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-08 09:46:22 +0200

    update gulpfile to concat scripts too

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var compass = require('gulp-compass');
 var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
     gulp.src('./_sass/default.scss').
@@ -11,12 +12,28 @@ gulp.task('styles', function() {
             sass: '_sass'
          })).
          pipe(minifyCss()).
-         pipe(gulp.dest('css'));
+         pipe(gulp.dest('dist'));
+});
+
+var scripts = [
+    './vendor/jquery/dist/jquery.min.js',
+    './vendor/bootstrap/dist/js/bootstrap.min.js',
+    './vendor/angular/angular.min.js',
+    './vendor/angular-route/angular-route.min.js',
+    './vendor/angular-translate/angular-translate.min.js',
+    './vendor/ng-ckeditor/ng-ckeditor.min.js',
+    './vendor/ng-file-upload/ng-file-upload-all.min.js'
+];
+gulp.task('scripts', function() {
+    gulp.src(scripts).
+         pipe(concat('libraries.js')).
+         pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
     gulp.watch('_sass/**/*.scss', ['styles']);
+    gulp.watch(scripts, ['scripts']);
 });
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'watch']);
 
