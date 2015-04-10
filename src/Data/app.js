ca25d1fc6fac03b03caf0afd76f77249ca8b7b03
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-10 10:04:06 +0200

    import drag

--- src/Data/app.js
+++ src/Data/app.js
@@ -3,11 +3,13 @@
 
 import {ListController} from './ListController';
 import {ItemController} from './ItemController';
+import {Drag} from './Drag';
 
 let ngModule = 'monad.Data';
 angular.module(ngModule, [])
     .controller('Data.ListController', ListController)
-    .controller('Data.ItemController', ItemController);
+    .controller('Data.ItemController', ItemController)
+    .directive('monadDrag', Drag.factory);
 
 export default ngModule;
 
