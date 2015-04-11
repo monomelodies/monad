4498384835fb5409a4ccf8f671cb518609344007
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-11 16:39:24 +0200

    let this handle injection

--- src/Data/ItemController.js
+++ src/Data/ItemController.js
@@ -5,9 +5,9 @@ import {ListController} from './ListController';
 
 class ItemController extends ListController {
 
-    constructor(app, Module, $routeParams) {
-        super(app, Module, $routeParams);
-        this.Service.find($routeParams).success(item => this.Model = item);
+    constructor(app, Module, $routeParams, $injector) {
+        super(app, Module, $routeParams, $injector);
+        this.Service.find($routeParams).success(item => this.Model = $injector.instantiate(this.Meta).$load(item));
     }
 
     page() {
@@ -15,7 +15,7 @@ class ItemController extends ListController {
 
 };
 
-ItemController.$inject = ['app', 'Module', '$routeParams'];
+ItemController.$inject = ['app', 'Module', '$routeParams', '$injector'];
 
 export {ItemController};
 
