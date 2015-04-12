60fa4d9f3855949626486192a27326270a5bff57
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-12 18:21:45 +0200

    register uploadcontroller, fix some names (can prolly go tho)

--- src/Data/app.js
+++ src/Data/app.js
@@ -3,12 +3,14 @@
 
 import {ListController} from './ListController';
 import {ItemController} from './ItemController';
+import {UploadController} from './UploadController';
 import {Drag} from './Drag';
 
 let ngModule = 'monad.Data';
 angular.module(ngModule, [])
-    .controller('Data.ListController', ListController)
-    .controller('Data.ItemController', ItemController)
+    .controller('monad.Data.ListController', ListController)
+    .controller('monad.Data.ItemController', ItemController)
+    .controller('monad.Data.UploadController', UploadController)
     .directive('monadDrag', Drag.factory);
 
 export default ngModule;
