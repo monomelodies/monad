3b8966e977489262a991a1ccf80496b3b4808a28
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-12 10:37:35 +0200

    add update method that reloads

--- src/Data/ItemController.js
+++ src/Data/ItemController.js
@@ -3,14 +3,21 @@
 
 import {ListController} from './ListController';
 
+let route;
+
 class ItemController extends ListController {
 
     constructor(Module, Model, Service, $routeParams, $route, $injector) {
         super(Module, Model, Service, $routeParams, $route, $injector);
         this.Service.find($routeParams).success(item => this.Model = $injector.instantiate(this.Meta).$load(item));
+        route = $route;
     }
 
     page() {
+    }
+
+    update() {
+        this.Model.$update().success(() => route.reload());
     }
 
 };
