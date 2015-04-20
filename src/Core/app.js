13911000126763e1a5ca152fd7b541aee1bee348
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-20 07:36:59 +0200

    turn off a few things during dev

--- src/Core/app.js
+++ src/Core/app.js
@@ -3,14 +3,15 @@
 
 import {normalizePost} from './normalizePost';
 import {Controller} from './Controller';
-import {default as Authentication} from '../Authentication/app';
-import {default as Home} from '../Home/app';
+//import {default as Authentication} from '../Authentication/app';
+//import {default as Home} from '../Home/app';
 import {default as Data} from '../Data/app';
 
 let ngModule = 'monad.Core';
-angular.module(ngModule, ['pascalprecht.translate', 'ngRoute', Authentication, Home, Data, 'ngCkeditor', 'ngSanitize', 'angularFileUpload'])
+//angular.module(ngModule, ['ng', 'pascalprecht.translate', 'ngRoute', Authentication, Home, Data, 'ngCkeditor', 'ngSanitize', 'angularFileUpload'])
+angular.module(ngModule, ['ng', 'pascalprecht.translate', 'ngRoute', Data, 'ngCkeditor', 'ngSanitize', 'angularFileUpload'])
     .run(['$http', normalizePost])
-    .controller('monad.Core.Controller', Controller);
+    .controller('RootController', Controller);
 
 export default ngModule;
 
