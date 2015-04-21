a928c6d6284799b1c8d7580231358091e9394fa0
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-21 10:38:35 +0200

    register directive

--- src/Data/app.js
+++ src/Data/app.js
@@ -5,12 +5,14 @@ import {UploadController} from './UploadController';
 import {ItemController} from './List/ItemController';
 import {Drag} from './Drag';
 import {Item} from './List/Item';
+import {Fieldset} from './Fieldset';
 
 let ngModule = 'monad.Data';
 angular.module(ngModule, ['ng'])
     .controller('monad.Data.UploadController', UploadController)
     .controller('ListItemController', ItemController)
     .directive('monadDataItem', Item.factory)
+    .directive('monadDataFieldset', Fieldset.factory)
     .directive('monadDrag', Drag.factory);
 
 export default ngModule;
