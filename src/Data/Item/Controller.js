605b6322dd7b3b8fbfe9c2dd236fb1631ec66f8b
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-21 08:42:24 +0200

    load current thingy (model is already instantiated by super()), and helper method to get template name

--- src/Data/Item/Controller.js
+++ src/Data/Item/Controller.js
@@ -7,13 +7,23 @@ class Controller extends Base {
 
     constructor(...args) {
         super(...args);
-//        this.Repository.find(args[1]).success(item => {
-//            this.item = this.instantiate(this.Model).$load(item);
-//        });
+        this.Repository.find(args[1]).success(item => {
+            this.item = this.Model.$load(item);
+        });
     }
 
     update() {
         this.Repository.update(this.item).success(this.reload);
+    }
+
+    templateFor(field) {
+        let t;
+        this.Schema.fields.map(f => {
+            if (f.key == field) {
+                t = f.widget;
+            }
+        });
+        return t;
     }
 
 };
