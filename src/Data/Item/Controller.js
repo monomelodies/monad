1fd8dc49dcf6081f240521a2af9305a306830eb7
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-21 10:38:20 +0200

    instantiation needed

--- src/Data/Item/Controller.js
+++ src/Data/Item/Controller.js
@@ -8,7 +8,7 @@ class Controller extends Base {
     constructor(...args) {
         super(...args);
         this.Repository.find(args[1]).success(item => {
-            this.item = this.Model.$load(item);
+            this.item = this.instantiate(this.Model).$load(item);
         });
     }
 
