532a4ce4c53417460f2045133edccaa325c7ea73
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-21 10:39:02 +0200

    directive

---
+++ src/Data/Fieldset.js
@@ -0,0 +1,44 @@
+
+"use strict";
+
+import {Module} from '../Module';
+
+class Fieldset {
+
+    constructor() {
+        this.restrict = 'E';
+        this.replace = true;
+        this.scope = {fieldset: '=', schema: '=', app: '=', model: '='};
+        this.templateUrl = 'monad/src/Data/fieldset.html';
+        this.controllerAs = 'ctrl';
+        this.controller = function($scope) {
+            this.templateFor = field => {
+                let t;
+                this.schema.fields.map(f => {
+                    if (f.key == field) {
+                        t = f.widget;
+                    }
+                });
+                return t;
+            };
+            this.editor = field => {
+                let t;
+                this.schema.fields.map(f => {
+                    if (f.key == field) {
+                        t = f.ckeditor;
+                    }
+                });
+                return t;
+            };
+        };
+        this.bindToController = true;
+    }
+
+    static factory() {
+        Fieldset.instance = new Fieldset();
+        return Fieldset.instance;
+    }
+};
+
+export {Fieldset};
+
