421352eac995bc1f6dffd6c83e320ba3221f9b45
Marijn Ophorst <marijn@sensimedia.nl>
2015-04-25 09:47:54 +0200

    add directives

--- src/Data/app.js
+++ src/Data/app.js
@@ -6,6 +6,11 @@ import {ItemController} from './List/ItemController';
 import {Drag} from './Drag';
 import {Item} from './List/Item';
 import {Fieldset} from './Fieldset';
+import {Pk} from './Pk';
+import {Text} from './Text';
+import {Slug} from './Slug';
+import {Textarea} from './Textarea';
+import {Html} from './Html';
 
 let ngModule = 'monad.Data';
 angular.module(ngModule, ['ng'])
@@ -13,6 +18,11 @@ angular.module(ngModule, ['ng'])
     .controller('ListItemController', ItemController)
     .directive('monadDataItem', Item.factory)
     .directive('monadDataFieldset', Fieldset.factory)
+    .directive('monadDataPk', () => new Pk())
+    .directive('monadDataText', () => new Text())
+    .directive('monadDataSlug', () => new Slug())
+    .directive('monadDataTextarea', () => new Textarea())
+    .directive('monadDataHtml', () => new Html())
     .directive('monadDrag', Drag.factory);
 
 export default ngModule;
