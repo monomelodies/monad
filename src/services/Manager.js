5be4e76934bbfe1c002bfe9dc5ddd1481ae86f91
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-02 09:08:52 +0200

    remove some old crap

--- src/services/Manager.js
+++ src/services/Manager.js
@@ -4,28 +4,6 @@
 class Manager {
 
     constructor() {
-        this.$data = {};
-        this.$fields = [];
-        this.$deleted = false;
-    }
-
-    $load(data = {}, recursive = true) {
-        this.$initial = data;
-        for (let key in data) {
-            var props = {enumerable: true, configurable: true};
-            if (!this.hasOwnProperty(key)) {
-                props.get = () => {
-                    return this.$data[key];
-                };
-                props.set = value => {
-                    this.$data[key] = value;
-                };
-            }
-            Object.defineProperty(this, key, props);
-            this.$data[key] = data[key];
-            this.$fields.push(key);
-        }
-        return this;
     }
 
     update(model) {
@@ -54,55 +32,6 @@ class Manager {
                 model.$initial[field] = model.$data[field];
             }
         }
-    }
-
-    get $new() {
-        return !('id' in this.$data && this.$data.id);
-    }
-
-    get $dirty() {
-        if (this.$deleted) {
-            return true;
-        }
-        for (let key in this) {
-            if (key.substring(0, 1) == '$') {
-                continue;
-            }
-            if (key in this.$data && this.$data[key] != this.$initial[key]) {
-                return true;
-            } else if (typeof this[key] == 'object' && this[key] != null) {
-                if ('map' in this[key]) {
-                    let dirty = false;
-                    this[key].map(elem => {
-                        if (typeof elem == 'object' && elem.$dirty || elem.$deleted) {
-                            dirty = true;
-                        }
-                    });
-                    if (dirty) {
-                        return true;
-                    }
-                } else if (this[key].$dirty || this[key].$deleted) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    $pluralize(field) {
-        field = field.replace(/y$/, 'ie');
-        return this[field + 's'];
-    }
-
-    $export() {
-        let data = {};
-        for (let prop in this) {
-            if (prop.substring(0, 1) == '$') {
-                continue;
-            }
-            data[prop] = this[prop];
-        }
-        return data;
     }
 
 };
