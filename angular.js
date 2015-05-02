bffcd812aaded3400bec19cbb4849a75ff14a0c2
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-02 07:45:09 +0200

    add some shizzle

--- angular.js
+++ angular.js
@@ -4,6 +4,11 @@
 import {Controller} from './src/Controller';
 import {Navigation} from './src/Navigation';
 import {default as Home} from './src/home/angular';
+import {LoginController} from './src/authentication/LoginController';
+import {default as normalizePost} from './src/core/normalizePost';
+import {default as listHeader} from './src/List/Header';
+import {default as listTable} from './src/List/Table';
+import {default as listRow} from './src/List/Row';
 
 let ngModule = 'monad.core';
 
@@ -19,7 +24,7 @@ function config($translateProvider, $translatePartialLoaderProvider, $routeProvi
             templateUrl: 'monad/template/home.html'
         }).
         when('/:language/login/', {
-            controller: 'moLoginController',
+            controller: LoginController,
             controllerAs: 'login',
             templateUrl: 'monad/template/login.html'
         });
@@ -31,8 +36,12 @@ function config($translateProvider, $translatePartialLoaderProvider, $routeProvi
 
 angular.module(ngModule, ['ng', 'ngRoute', 'pascalprecht.translate', 'ngCkeditor', 'ngSanitize', 'angularFileUpload', 'ui.bootstrap', Home])
     .config(['$translateProvider', '$translatePartialLoaderProvider', '$routeProvider', '$locationProvider', config])
+    .run(['$http', normalizePost])
     .controller('moController', Controller)
     .service('moNavigation', Navigation)
+    .directive('moListHeader', listHeader)
+    .directive('moListTable', listTable)
+    .directive('moListRow', listRow)
 ;
 
 export default ngModule;
