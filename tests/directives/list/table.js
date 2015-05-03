07fc21ea43ef91cfd8d66429a94ec1364a8ce172
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-03 15:48:52 +0200

    test table directive

---
+++ tests/directives/list/table.js
@@ -0,0 +1,36 @@
+
+"use strict";
+
+import {default as Monad} from '../../../angular';
+
+describe('directive: mo-list-table', () => {
+    let element;
+    let scope;
+    let compile;
+    let items = [{id: 1, txt: 'foo'}, {id: 2, txt: 'bar'}];
+    let tpl = angular.element(`<mo-list-table module="'test'" items="items" columns="['id', 'txt']"></mo-list-table>`);
+
+    beforeEach(angular.mock.module('TEMPLATES'));
+    beforeEach(angular.mock.module(Monad));
+    beforeEach(inject(($rootScope, $compile) => {
+        scope = $rootScope;
+        compile = $compile;
+    }));
+
+    it('should insert the table into the element', () => {
+        scope.$apply(() => {
+            scope.items = items;
+            scope.erm = 'bla';
+            scope.table = {items};
+        });
+        element = compile(tpl)(scope);
+        scope.$digest();
+        expect(element.find('tbody tr').length).toBe(2);
+        expect(element.find('thead').length).toBe(1);
+        expect(element.find('tbody td').eq(0).text().replace(/\s+/g, '')).toEqual('1');
+        expect(element.find('tbody td').eq(1).text().replace(/\s+/g, '')).toEqual('foo');
+        expect(element.find('tbody td').eq(2).text().replace(/\s+/g, '')).toEqual('2');
+        expect(element.find('tbody td').eq(3).text().replace(/\s+/g, '')).toEqual('bar');
+    });
+});
+
