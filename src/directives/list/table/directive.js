92d0747ec46b62b4edd14d1faf91cedce31b3d6e
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-03 15:47:43 +0200

    nevermind controllerAs for now, it's in an isolate scope anyway

--- src/directives/list/table/directive.js
+++ src/directives/list/table/directive.js
@@ -5,9 +5,8 @@ export default () => {
     return {
         restrict: 'E',
         templateUrl: 'monad/src/directives/list/table/template.html',
-        scope: {module: '=', items: '=', columns: '='},
+        scope: {module: '=', items: '=', columns: '=', path: '='},
         controller,
-        controllerAs: 'table',
         bindToController: true
     };
 };
