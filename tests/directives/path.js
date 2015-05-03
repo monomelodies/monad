cce8f6ed59def8060575d09f1feed8033ad8cc5b
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-03 15:48:46 +0200

    test path directive

---
+++ tests/directives/path.js
@@ -0,0 +1,32 @@
+
+"use strict";
+
+import {default as Monad} from '../../angular';
+
+describe('directive: mo-path', () => {
+    let element;
+    let scope;
+    let compile;
+    let items = [{id: 1, txt: 'foo'}, {id: 2, txt: 'bar'}];
+    let tpl = angular.element(`<a mo-path="path" arguments="{foo: 'bar'}">{{txt}}</a>`);
+
+    beforeEach(angular.mock.module('TEMPLATES'));
+    beforeEach(angular.mock.module(Monad));
+    beforeEach(inject(($rootScope, $compile, $routeParams) => {
+        $routeParams.language = 'en';
+        scope = $rootScope;
+        compile = $compile;
+    }));
+
+    it('should add an href to the element and interpolate contents', () => {
+        scope.$apply(() => {
+            scope.path = '/:language/something/:foo/';
+            scope.txt = 'link';
+        });
+        element = compile(tpl)(scope);
+        scope.$digest();
+        expect(element.attr('href')).toBe('#/en/something/bar/');
+        expect(element.text()).toBe('link');
+    });
+});
+
