93069a63468051f4fc008f3839c0aae64d6b78ed
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-05 10:27:35 +0200

    load item, and tentatively add methods from old controller

--- src/controllers/UpdateController.js
+++ src/controllers/UpdateController.js
@@ -1,8 +1,41 @@
 
 "use strict";
 
-class UpdateController {
+import {DataController} from './DataController';
+
+class UpdateController extends DataController {
+
+    constructor($routeParams, ...args) {
+        super(...args);
+        this.manager.find($routeParams).success(item => this.item = item);
+    }
+    
+    update(item) {
+        let result;
+        if (result = this.Repository.update(item)) {
+            result.success(this.reload);
+        }
+    }
+    
+    ['delete'](item) {
+        // TODO: confirm?
+        item.$deleted = true;
+        return this.Repository['delete'](item);
+    }
+    
+    templateFor(field) {
+        let t;
+        this.Schema.fields.map(f => {
+            if (f.key == field) {
+                t = f.widget;
+            }
+        });
+        return t;
+    }
+
 };
+
+UpdateController.$inject = ['$routeParams'].concat(DataController.$inject);
 
 export {UpdateController};
 
