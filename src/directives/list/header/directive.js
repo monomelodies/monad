3f7d0bef597b055638c15c5b3f6b9b777b549960
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-07 02:50:02 +0200

    transclude

--- src/directives/list/header/directive.js
+++ src/directives/list/header/directive.js
@@ -3,7 +3,8 @@ export default () => {
     return {
         restrict: 'E',
         templateUrl: 'monad/src/directives/list/header/template.html',
-        scope: {create: '=', module: '='}
+        scope: {create: '=', module: '='},
+        transclude: true
     };
 };
 
