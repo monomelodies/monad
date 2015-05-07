c1e4273596f60c5a0fcdad3b62de2066857d5c6e
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-07 12:08:43 +0200

    let this be its own controller

--- src/controllers/ListController.js
+++ src/controllers/ListController.js
@@ -1,24 +1,42 @@
 
 "use strict";
 
-import {CrudController} from './CrudController';
+let params;
 
-class ListController extends CrudController {
+class ListController {
 
-    constructor(...args) {
-        super(...args);
+    constructor($route, $routeParams, $translatePartialLoader) {
+        if ($route.current && $route.current.locals) {
+            for (let p in $route.current.locals) {
+                if (p.substring(0, 1) == '$') {
+                    continue;
+                }
+                this[p] = $route.current.locals[p];
+            }
+        }
+        $translatePartialLoader.addPart(this.module);
+        this.$new = new this.Manager.model();
         this.items = [];
-        this.page(args[1]);
+        params = $routeParams;
+        this.page = params.page || 1;
     }
 
-    page(params) {
+    get page() {
+        return this._page;
+    }
+
+    set page(page) {
+        console.log(page);
+        this._page = page;
         while (this.items.length) {
             this.items.pop();
         }
-        this.Manager.list(params).success(items => items.map(item => this.items.push(item)));
-    }
+        this.Manager.list(params, {offset: (page - 1) * 10}).success(items => items.map(item => this.items.push(item)));
+    };
 
 };
+
+ListController.$inject = ['$route', '$routeParams', '$translatePartialLoader'];
 
 export {ListController};
 
