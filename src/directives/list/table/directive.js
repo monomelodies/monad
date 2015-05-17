744b75e43c6c56b866097f385adb6f77f55d75df
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-17 10:43:05 +0200

    live and learn: use @ where appropriate, and eval columns attr which should be an array

--- src/directives/list/table/directive.js
+++ src/directives/list/table/directive.js
@@ -5,8 +5,11 @@ export default () => {
     return {
         restrict: 'E',
         templateUrl: 'monad/src/directives/list/table/template.html',
-        scope: {list: '=', columns: '=', path: '='},
+        scope: {list: '=', columns: '@', path: '@'},
         controller,
+        link: (scope, elem, attrs) => {
+            scope.columns = scope.eval(attrs.columns);
+        },
         bindToController: true
     };
 };
