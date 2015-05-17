31910a1f592e25f3390c5d50e2895982bdb23b8b
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-17 14:01:54 +0200

    pass module

--- src/directives/list/table/directive.js
+++ src/directives/list/table/directive.js
@@ -3,12 +3,14 @@ import {Controller as controller} from './controller';
 
 export default () => {
     return {
+        require: '^moList',
         restrict: 'E',
         templateUrl: 'monad/src/directives/list/table/template.html',
-        scope: {module: '@', rows: '=', path: '@', total: '@', page: '='},
+        scope: {rows: '=', path: '@', total: '@', page: '='},
         controller,
-        link: (scope, elem, attrs) => {
+        link: (scope, elem, attrs, ctrl) => {
             scope.columns = scope.$eval(attrs.columns);
+            scope.module = ctrl.module;
         },
         bindToController: true
     };
