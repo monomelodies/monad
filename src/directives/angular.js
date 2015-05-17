f94bf29a80dd340bae6d9893251ad24c34dc52a8
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-17 17:05:21 +0200

    bootstrapper for directives

---
+++ src/directives/angular.js
@@ -0,0 +1,23 @@
+
+"use strict";
+
+import {default as ListHeader} from './list/header/directive';
+import {default as ListTable} from './list/table/directive';
+import {default as Path} from './path/directive';
+import {default as Field} from './field/directive';
+import {default as Update} from './update/directive';
+import {default as Draggable} from './draggable/directive';
+import {default as List} from './list';
+
+angular.module('monad.directives', ['ng'])
+    .directive('moList', List)
+    .directive('moListHeader', ListHeader)
+    .directive('moListTable', ListTable)
+    .directive('moPath', Path)
+    .directive('moField', Field)
+    .directive('moUpdate', Update)
+    .directive('moDraggable', Draggable)
+    ;
+
+export default 'monad.directives';
+
