5011535074bcb1961cb72aaeeb8feb551c669ae2
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-18 17:21:11 +0200

    setup for an actual monad global object

---
+++ src/Monad.js
@@ -0,0 +1,30 @@
+
+"use strict";
+
+console.log('importing?');
+import {Module} from './classes/Module';
+
+let ngmod;
+let defined = {};
+
+class Monad {
+
+    application(prefix, deps = [], configFn = undefined) {
+        if (ngmod == undefined) {
+            ngmod = new Module(prefix, angular.module('monad', deps.concat(['monad.core']), configFn));
+        } else {
+            throw "Sorry, you can only call `monad.application` once!";
+        }
+        return ngmod;
+    }
+
+    module(prefix, name, deps = [], configFn = undefined) {
+        if (!defined[name]) {
+            defined[name] = new Module(prefix, angular.module(name, deps.concat(['monad'], configFn)));
+        }
+        return defined[name];
+    }
+};
+
+export {Monad};
+
