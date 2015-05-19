0c8b8ab407387821ed7dcb42588538748322f8d1
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-19 11:41:26 +0200

    no subdir needed

--- src/directives/angular.js
+++ src/directives/angular.js
@@ -3,7 +3,7 @@
 
 import {default as ListHeader} from './list/header/directive';
 import {default as ListTable} from './list/table/directive';
-import {default as Path} from './path/directive';
+import {default as Path} from './path';
 import {default as Field} from './field/directive';
 import {default as Update} from './update/directive';
 import {default as Draggable} from './draggable/directive';
