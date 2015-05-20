a5fd2f27acfcc1e924568a5f75ce1a8a59fa46e7
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-20 07:40:08 +0200

    implement test

--- tests/core.spec.js
+++ tests/core.spec.js
@@ -3,16 +3,14 @@
 
 describe("Main entry point for Monad", () => {
 
-    /*
-    describe("Monad module handling", () => {
-        it("Registers", () => {
-            expect(Module.register('Test', {})).toBe('Test');
+    describe("Monad global object", () => {
+        it("Registers the application", () => {
+            expect(monad.application('test').name).toBe('monad');
         });
-        it("Can be retrieved", () => {
-            expect(Module.retrieve('Test').app).toBe('Test');
+        it("Can only load once", () => {
+            expect(() => monad.application('test')).toThrow();
         });
     });
-    */
 
 });
 
