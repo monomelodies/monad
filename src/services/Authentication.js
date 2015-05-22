e5f69d39e916685acaf07850f71de9f21fa77142
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-22 20:43:06 +0200

    throw errors

--- src/services/Authentication.js
+++ src/services/Authentication.js
@@ -4,15 +4,19 @@
 export class Authentication {
 
     read() {
+        throw "Authentication.read must be replaced by your custom implementation.";
     }
     
     login(username, password) {
+        throw "Authentication.login must be replaced by your custom implementation.";
     }
     
     logout() {
+        throw "Authentication.logout must be replaced by your custom implementation.";
     }
     
     isAuthenticated() {
+        return false;
     }
 
 };
