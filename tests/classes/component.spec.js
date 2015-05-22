2a5cf2867e0fbd70916b300a2ee8f87d70d51acd
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-22 09:27:32 +0200

    setup component test (todo: think how to do that...)

---
+++ tests/classes/component.spec.js
@@ -0,0 +1,12 @@
+
+/**
+ * TODO: think of a sane test implementation for this...
+ */
+describe("class: Component", () => {
+
+    it("has a dummy test :)", () => {
+        expect(true).toEqual(true);
+    });
+
+});
+
