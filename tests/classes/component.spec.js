a5483dd0ce410d823d0ebb2acbf16e2386656a68
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-26 11:55:22 +0200

    use name property

--- tests/classes/component.spec.js
+++ tests/classes/component.spec.js
@@ -7,7 +7,7 @@ describe("class: Component", () => {
     let test = monad.component('foo/bar/baz/buz').manager(() => {});
 
     it("normalizes names correctly", () => {
-        expect(test.$manager).toEqual('fooBarBazBuzManager');
+        expect(test.$manager.name).toEqual('fooBarBazBuzManager');
     });
 
 });
