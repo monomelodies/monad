7509febe666a27c693764efb1bafbb3c8ea3230f
Marijn Ophorst <marijn@sensimedia.nl>
2015-05-26 22:26:18 +0200

    set language here

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -3,23 +3,25 @@
 
 let service;
 let loc;
+let language;
 
 class LoginController {
 
-    constructor($location, Auth) {
+    constructor($location, Auth, moLanguage) {
         service = Auth;
         loc = $location;
+        language = moLanguage;
     }
 
-    attempt(language) {
+    attempt() {
         service.login(this.username, this.password).success(result => {
-            loc.path('/' + language + '/');
+            loc.path('/' + language.current + '/');
         });
     }
 
 };
 
-LoginController.$inject = ['$location', 'moAuthentication'];
+LoginController.$inject = ['$location', 'moAuthentication', 'moLanguage'];
 
 export {LoginController};
 
