458f3fa6cbe19fcf396a6fb374f0357604d14214
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-02 16:26:26 +0200

    this can go

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,6 @@ module.exports = function(config) {
     frameworks: ['browserify', 'jasmine'],
     files: [
         'dist/libraries.js',
-        'dist/bundle.js',
         'node_modules/angular-mocks/angular-mocks.js',
         'tests/setup.js',
         'tests/**/*.spec.js',
