1d492f0c9ae8fa73b627e257ebcf9060cc21ee02
Marijn Ophorst <marijn@sensimedia.nl>
2015-06-02 16:29:25 +0200

    update tests: monad is now global on window again

--- tests/classes/component.spec.js
+++ tests/classes/component.spec.js
@@ -3,7 +3,7 @@ import {monad} from '../../src/angular';
 
 describe("class: Component", () => {
 
-    let test = monad.component('foo/bar/baz/buz').manager(() => {});
+    let test = window.monad.component('foo/bar/baz/buz').manager(() => {});
 
     it("normalizes names correctly", () => {
         expect(test.$manager.name).toEqual('fooBarBazBuzManager');
--- tests/core.spec.js
+++ tests/core.spec.js
@@ -8,14 +8,14 @@ describe("Monad global application and component handler", () => {
 
     describe("Monad global object", () => {
 
-        monad.component('testComponent');
-        let app = monad.application('test');
+        window.monad.component('testComponent');
+        let app = window.monad.application('test');
 
         it("Registers the application", () => {
             expect(app.name).toBe('monad');
         });
         it("Can only load once", () => {
-            expect(() => monad.application('test')).toThrow();
+            expect(() => window.monad.application('test')).toThrow();
         });
         it("automatically receive component dependencies", () => {
 //            console.log(Component.get('monad'));
