fa2da85c09e84890f03ba8c11c1b1f0859147b88
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-07 12:15:28 +0200

    this is moved to a directive

--- src/controllers/ListController.js
+++ src/controllers/ListController.js
@@ -53,33 +53,6 @@ class ListController {
         this.Manager.paginate(page, params).success(items => this.items = items);
     }
 
-    /**
-     * Method allowing item deletion directly from a list:
-     */
-    ['delete'](item) {
-        let modalInstance = modal.open({
-            template: `<div class="modal-header"><h3 class="modal-title">{{'monad.delete.title' | translate}}</h3></div>
-<div class="modal-body">
-    {{'monad.delete.body' | translate}}
-</div>
-<div class="modal-footer">
-    <button class="btn btn-primary" ng-click="ok()">{{'monad.delete.ok' | translate}}</button>
-    <button class="btn btn-warning" ng-click="cancel()">{{'monad.delete.cancel' | translate}}</button>
-</div>`,
-            controller: ['$scope', '$modalInstance', ($scope, $modalInstance) => {
-                $scope.ok = () => {
-                    this.Manager['delete'](item);
-                    $modalInstance.close(item);
-                    route.reset();
-                    route.reload();
-                };
-                $scope.cancel = () => {
-                    $modalInstance.dismiss('cancel');
-                };
-            }]
-        });
-    }
-
 };
 
 ListController.$inject = ['$scope', '$route', '$modal', '$translatePartialLoader', '$injector'];
