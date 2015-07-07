36e052b808cd9149f19fa045033a246ad9d7131f
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-07 18:42:50 +0200

    add submenus... todo: fix infDigest error in angular (caused by get-accessor?)

--- src/services/Navigation.js
+++ src/services/Navigation.js
@@ -5,9 +5,30 @@ let paths = {};
 let loc;
 let auth;
 
+class Menu {
+}
+
 class Navigation {
 
     constructor($location, Authentication, $injector) {
+
+        function access(component) {
+            let authenticate;
+            if (component.defaults.list
+                && component.defaults.list.resolve
+                && component.defaults.list.resolve.Authentication
+            ) {
+                authenticate = $injector.instantiate(component.defaults.list.resolve.Authentication);
+            } else {
+                authenticate = Authentication;
+            }
+            if (authenticate && authenticate.check) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
         loc = $location;
         auth = Authentication;
         for (let menu in paths) {
@@ -18,20 +39,27 @@ class Navigation {
                         items.push(path);
                         return;
                     }
-                    let component = monad.component(path.component);
-                    let authenticate;
-                    if (component.defaults.list
-                        && component.defaults.list.resolve
-                        && component.defaults.list.resolve.Authentication
-                    ) {
-                        authenticate = $injector.instantiate(component.defaults.list.resolve.Authentication);
+                    if (path.component) {
+                        let component = monad.component(path.component);
+                        if (access(component)) {
+                            items.push(path);
+                            if ('$manager' in component) {
+                                let manager = $injector.get(component.$manager.name);
+                                path.notifications = manager.notify;
+                            }
+                        }
                     } else {
-                        authenticate = auth;
-                    }
-                    if (authenticate && authenticate.check) {
-                        items.push(path);
-                        let manager = $injector.get(component.$manager.name);
-                        path.notifications = manager.notify;
+                        // Submenu.
+                        let sub = {label: path.label, items: []};
+                        path.items.map(item => {
+                            let component = monad.component(item.component);
+                            if (access(component)) {
+                                sub.items.push(item);
+                            }
+                        });
+                        if (sub.items.length) {
+                            items.push(sub);
+                        }
                     }
                 });
                 return items;
@@ -40,9 +68,35 @@ class Navigation {
     }
 
     static register(component, menu, url, label) {
-        let selected = false;
+        let work;
+        if (typeof menu == 'string') {
+            let m = {};
+            m[menu] = null;
+            menu = m;
+        }
+        let sub;
+        for (let key in menu) {
+            sub = menu[key];
+            menu = key;
+        }
         paths[menu] = paths[menu] || [];
-        paths[menu].push({component, url, label, selected});
+        let selected = false;
+        let found = false;
+        let next = {component, url, label, selected};
+        paths[menu].map(item => {
+            if (item.label == sub) {
+                item.items.push(next);
+                found = true;
+            }
+        });
+        if (!found && sub) {
+            let subitem = new Menu;
+            subitem.label = sub;
+            subitem.items = [next];
+            paths[menu].push(subitem);
+        } else {
+            paths[menu].push(next);
+        }
     }
 
     current() {
