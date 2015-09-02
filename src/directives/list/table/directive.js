af5a92175d1d3c8b92a60cdbca4e4b1478993b36
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-02 16:28:17 +0200

    take columns from transcluded directive

--- src/directives/list/table/directive.js
+++ src/directives/list/table/directive.js
@@ -4,18 +4,25 @@ export default () => {
         require: '^moList',
         restrict: 'E',
         templateUrl: '../monad/directives/list/table/template.html',
-        scope: {columns: '=', rows: '=', total: '@', list: '=', templates: '='},
-        controller: ['$scope', '$route', function($scope, $route) {
+        scope: {rows: '=', total: '@', list: '=', templates: '='},
+        controller: ['$scope', '$route', '$transclude', '$element', function($scope, $route, $transclude, $element) {
             this.refresh = promise => promise.then(() => {
                 $route.reset();
                 this.list.page = this.list.page;
             });
+            this.columns = [];
+            let hdrs = $transclude().find('th');
+            hdrs.each((i, hdr) => {
+                this.columns.push(angular.element(hdr).attr('property'));
+            });
+            $element.find('thead tr').append(hdrs);
         }],
         controllerAs: 'tbody',
         link: (scope, elem, attrs, ctrl) => {
             scope.tbody.module = ctrl.module;
         },
-        bindToController: true
+        bindToController: true,
+        transclude: true
     };
 };
 
