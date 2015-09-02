7220f16ab82abf96f91866287326cfc25c218ec1
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-02 16:29:39 +0200

    update test to use new template form, and check if headers are injected

--- tests/directives/list/table.spec.js
+++ tests/directives/list/table.spec.js
@@ -8,7 +8,12 @@ describe('directive: mo-list-table', () => {
     let items = [{id: 1, txt: 'foo'}, {id: 2, txt: 'bar'}];
     let tpl = angular.element(`
 <mo-list module="test">
-    <mo-list-table columns="['id', 'txt']" total="1" page="page" rows="items"></mo-list-table>
+    <mo-list-table total="1" page="page" rows="items">
+        <table><tr>
+            <th property="id">ID</th>
+            <th property="txt">Text</th>
+        </tr></table>
+    </mo-list-table>
 </mo-list>
     `);
 
@@ -26,6 +31,7 @@ describe('directive: mo-list-table', () => {
         scope.$digest();
         expect(element.find('tbody tr').length).toBe(2);
         expect(element.find('thead').length).toBe(1);
+        expect(element.find('thead th').length).toBe(2);
         expect(element.find('tbody td').eq(0).text().replace(/\s+/g, '')).toEqual('1');
         expect(element.find('tbody td').eq(1).text().replace(/\s+/g, '')).toEqual('foo');
         expect(element.find('tbody td').eq(2).text().replace(/\s+/g, '')).toEqual('2');
