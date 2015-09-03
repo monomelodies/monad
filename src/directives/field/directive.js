30d00e0e73242c0d91d350277422ab92fba224c0
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-03 08:38:15 +0200

    this is better done simply in the transcluded content

--- src/directives/field/directive.js
+++ src/directives/field/directive.js
@@ -3,13 +3,12 @@ export default () => {
     return {
         restrict: 'E',
         template: `<div class="form-group">
-    <label>{{label}}</label>
     <span ng-transclude></span>
 </div>`,
         link: (scope, elem, attrs) => {
             elem.find('input, textarea, select').addClass('form-control');
         },
-        scope: {label: '='},
+        scope: {},
         transclude: true
     };
 };
