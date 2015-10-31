45c25683423b3f780b2a92ed4a937e08705a00a2
Marijn Ophorst <marijn@sensimedia.nl>
2015-10-31 15:36:09 +0100

    this is neater

--- src/directives/list/table/directive.js
+++ src/directives/list/table/directive.js
@@ -1,35 +1,43 @@
 
+class controller {
+
+    constructor($scope, $route, $transclude, $element) {
+        this.refresh = promise => promise.then(() => {
+            $route.reset();
+            this.list.page = this.list.page;
+        });
+        this.columns = [];
+        if (this.items && this.items.length) {
+            for (let key in this.items[0]) {
+                this.columns.push(key);
+            }
+        }
+        let hdrs = $transclude().find('th');
+        if (hdrs.length) {
+            this.columns = [];
+            hdrs.each((i, hdr) => {
+                this.columns.push(angular.element(hdr).attr('property'));
+            });
+            $element.find('thead tr').append(hdrs);
+        } else {
+            let h = $element.find('thead tr');
+            this.columns.map(col => {
+                console.log(col);
+            //    h.append('<th>' + col + '</th>');
+            });
+        }
+    }
+}
+
+controller.$inject = ['$scope', '$route', '$transclude', '$element'];
+
 export default () => {
     return {
         require: '^moList',
         restrict: 'E',
         templateUrl: '../monad/directives/list/table/template.html',
         scope: {rows: '=', templates: '='},
-        controller: ['$scope', '$route', '$transclude', '$element', function($scope, $route, $transclude, $element) {
-            this.refresh = promise => promise.then(() => {
-                $route.reset();
-                this.list.page = this.list.page;
-            });
-            this.columns = [];
-            if (this.items && this.items.length) {
-                for (let key in this.items[0]) {
-                    this.columns.push(key);
-                }
-            }
-            let hdrs = $transclude().find('th');
-            if (hdrs.length) {
-                this.columns = [];
-                hdrs.each((i, hdr) => {
-                    this.columns.push(angular.element(hdr).attr('property'));
-                });
-                $element.find('thead tr').append(hdrs);
-            } else {
-                let h = $element.find('thead tr');
-                this.columns.map(col => {
-                    h.append('<th>' + col + '</th>');
-                });
-            }
-        }],
+        controller,
         controllerAs: 'tbody',
         link: (scope, elem, attrs, ctrl) => {
             scope.tbody.component = ctrl.component;
