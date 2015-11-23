40dfadda6fa7cc2cce7829a0676ef70c9efbfc10
Marijn Ophorst <marijn@sensimedia.nl>
2015-11-23 08:42:40 +0100

    add a real controller that prefills our model

--- src/directives/update/directive.js
+++ src/directives/update/directive.js
@@ -21,7 +21,7 @@ export default () => {
             item: '=',
             component: '='
         },
-        controller: function() {},
+        controller,
         controllerAs: 'update',
         bindToController: true,
         compile: (element, attrs) => {
@@ -33,4 +33,29 @@ export default () => {
         }
     };
 };
+
+class controller {
+
+    constructor($transclude, $timeout) {
+        if (this.item.$new) {
+            // New items have no notion of their fields yet,
+            // so we need to manually build the properties
+            // by analysing the transcluded schema.
+            $timeout(() => {
+                let test = angular.element('#mo_update_form').html().match(/ng-model="crud\.item\.(.*?)"/g);
+                let props = {};
+                if (test) {
+                    for (let i = 0; i < test.length; i++) {
+                        let item = test[i].replace('ng-model="crud.item.', '').replace('"', '');
+                        props[item] = null;
+                    }
+                }
+                this.item = this.item.constructor.$create(props);
+            }, 100);
+        }
+    }
+
+}
+
+controller.$inject = ['$transclude', '$timeout'];
 
