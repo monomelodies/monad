da99a3d2ec4236c2b55210e855d99b333df2b4f4
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-11 07:59:06 +0100

    handle message removal too

--- src/services/Report.js
+++ src/services/Report.js
@@ -5,8 +5,14 @@ let _messages = [];
 
 export default class Report {
 
-    add(body, type, scope, timeout) {
-        _messages.push({body, type, scope, timeout});
+    add(body, type, scope, promise) {
+        let msg = {body, type, scope};
+        let i = _messages.length;
+        _messages.push(msg);
+        promise.then(
+            () => _messages.splice(i, 1),
+            () => _messages.splice(i, 1)
+        );
     }
 
     get messages() {
