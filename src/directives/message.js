0cb7f5fcddb9e16dd5004e258aadd68c3ae358e0
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-12 10:42:09 +0100

    better message directive

--- src/directives/message.js
+++ src/directives/message.js
@@ -1,14 +1,22 @@
 
 "use strict";
 
+class controller {
+
+    get data() {
+        return this._msg.scope;
+    }
+
+}
+
 export default ['$compile', $compile => ({
-    restrict: 'A',
-    controller: function () {},
+    restrict: 'E',
+    controller,
     controllerAs: 'msg',
     bindToController: true,
-    scope: {data: '=scope', body: '='},
+    scope: {_msg: '=data'},
     link: (scope, elem, attrs) => {
-        let html = angular.element(scope.msg.body);
+        let html = angular.element('<div>' + scope.msg._msg.body + '</div>');
         elem.append($compile(html)(scope));
     }
 })];
