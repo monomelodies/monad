aab62ea6de8526d05580e782e13cb85cb681bc3a
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-13 22:31:51 +0100

    restore the old entry point

---
+++ monad.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+import 'babel-polyfill/dist/polyfill.js';
+import 'angular/angular.js';
+import 'angular-ui-bootstrap/ui-bootstrap.js';
+import 'angular-ui-bootstrap/ui-bootstrap-tpls.js';
+import 'angular-route/angular-route.js';
+import 'angular-sanitize/angular-sanitize.js';
+import 'angular-animate/angular-animate.js';
+import 'autofill-event/autofill-event.js';
+import 'angular-gettext/dist/angular-gettext.js';
+
+import monad from './src/angular';
+
+export default monad;
+
