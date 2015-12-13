856e91778adaf5de9cc195c44357a042a7d0f00d
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-13 18:40:40 +0100

    load these here

--- src/angular.js
+++ src/angular.js
@@ -1,6 +1,16 @@
 
 "use strict";
 
+import '../node_modules/babel-polyfill/dist/polyfill.js';
+import '../bower_components/angular/angular.js';
+import '../bower_components/angular-bootstrap/ui-bootstrap.js';
+import '../bower_components/angular-bootstrap/ui-bootstrap-tpls.js';
+import '../bower_components/angular-route/angular-route.js';
+import '../bower_components/angular-sanitize/angular-sanitize.js';
+import '../bower_components/angular-animate/angular-animate.js';
+import '../bower_components/autofill-event/src/autofill-event.js';
+import '../bower_components/angular-gettext/dist/angular-gettext.js';
+
 import Monad from './classes/Monad';
 import RootController from './controllers/RootController';
 import HomeController from './controllers/HomeController';
