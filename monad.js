ed4289a5948e4b5672c8342aa3808b155cbee26e
Marijn Ophorst <marijn@sensimedia.nl>
2016-01-02 10:49:37 +0100

    fix imports properly

--- monad.js
+++ monad.js
@@ -1,16 +1,17 @@
 
 "use strict";
 
-import 'babel-polyfill/dist/polyfill.js';
-import 'angular/angular.js';
-import 'angular-ui-bootstrap/ui-bootstrap.js';
+import 'babel-polyfill';
+import 'angular';
+import 'angular-ui-bootstrap';
 import 'angular-ui-bootstrap/ui-bootstrap-tpls.js';
-import 'angular-route/angular-route.js';
-import 'angular-sanitize/angular-sanitize.js';
-import 'angular-animate/angular-animate.js';
-import 'autofill-event/autofill-event.js';
-import 'angular-gettext/dist/angular-gettext.js';
-
+import 'angular-route';
+import 'angular-sanitize';
+import 'angular-animate';
+import 'autofill-event';
+import 'angular-gettext';
+import './i18n';
+import './templates';
 import monad from './src/angular';
 
 export default monad;
