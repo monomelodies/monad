05b5160d4a18ba53e95fb613144ee106d0db90c8
Marijn Ophorst <marijn@sensimedia.nl>
2016-01-04 08:12:55 +0100

    improve docs for authentication "interface"

--- src/services/Authentication.js
+++ src/services/Authentication.js
@@ -7,32 +7,38 @@
 export default class Authentication {
 
     /**
-     * _Should_ return a promise checking the current authentication status.
+     * _Must_ return a promise checking the current authentication status.
+     *
+     * @return promise
      */
     ['status']() {
         throw "Authentication.status must be replaced by your custom implementation.";
     }
     
     /**
-     * _Should_ attempt to login a user. Username/password is an example, you
-     * can implement whatever login scheme you need.
+     * _Must_ attempt to login a user. The default for generic login is to
+     * `username` and `password` credentials, but by overriding the login
+     * template you can implement whatever login scheme you need.
      *
-     * @param string username Example username parameter.
-     * @param string password Example password parameter.
+     * @param object credentials Hash of credentials.
+     * @return promise
      */
     attempt(username, password) {
         throw "Authentication.attempt must be replaced by your custom implementation.";
     }
     
     /**
-     * _Should_ revoke the current user's rights.
+     * _Must_ revoke the current user's rights, either on the current component
+     * or globally (depending on situation/application needs).
+     *
+     * @return promise
      */
     revoke() {
         throw "Authentication.revoke must be replaced by your custom implementation.";
     }
     
     /**
-     * _Should_ return true (authenticated) or false (not authenticated)
+     * _Must_ return true (authenticated) or false (not authenticated)
      *  depending on your implementation and the user's status.
      */
     get check() {
