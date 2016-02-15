4cdf3f89d1692c7bd278b92ba1fcb1ae35da73ed
Marijn Ophorst <marijn@sensimedia.nl>
2016-02-15 19:24:26 +0100

    cleanup, move to components

--- src/directives/angular.js
+++ src/directives/angular.js
@@ -1,31 +1,19 @@
 
 "use strict";
 
-import ListHeader from './list/header/directive';
-import ListTable from './list/table/directive';
 import Delete from './list/table/delete';
 import Path from './path';
 import Field from './field/directive';
-import Update from './update/directive';
 import DragDrop from './dragDrop';
-import List from './list';
 import Slug from './slug';
 import Message from './message';
-import Login from './login';
 
-angular.module('monad.directives', ['ng'])
-    .directive('moList', List)
-    .directive('moListHeader', ListHeader)
-    .directive('moListTable', ListTable)
+angular.module('monad.directives', [])
     .directive('moDelete', Delete)
     .directive('moPath', Path)
     .directive('moField', Field)
-    .directive('moUpdate', Update)
     .directive('moDragDrop', DragDrop)
     .directive('moSlug', Slug)
     .directive('moMessage', Message)
-    .directive('moLogin', Login)
     ;
-
-export default 'monad.directives';
 
