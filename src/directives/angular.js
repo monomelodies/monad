5f06f8e9531ade92fe22919ac5a0ce259e2c5923
Marijn Ophorst <marijn@sensimedia.nl>
2016-02-19 07:11:37 +0100

    deprecated/remove moPath directive

--- src/directives/angular.js
+++ src/directives/angular.js
@@ -1,14 +1,12 @@
 
 "use strict";
 
-import Path from './path';
 import Field from './Field';
 import DragDrop from './dragDrop';
 import Slug from './slug';
 import Message from './message';
 
 angular.module('monad.directives', [])
-    .directive('moPath', Path)
     .directive('moField', Field)
     .directive('moDragDrop', DragDrop)
     .directive('moSlug', Slug)
