29cd58194a5eab37a5bc4dd217c5b62b505090ca
Marijn Ophorst <marijn@sensimedia.nl>
2016-07-06 15:29:00 +0200

    rebuild

--- dist/monad.js
+++ dist/monad.js
@@ -49719,237 +49719,6 @@ ListController.$inject = ['moDelete', '$rootScope'];
 
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var wm = new WeakMap();
-var promise = new WeakMap();
-
-/**
- * A data object with dirty checking and other virtual helpers.
- */
-
-var Model = function () {
-
-    /**
-     * Class constructor. Pass the initial key/values as data. This should be
-     * an Angular resource object.
-     */
-
-    function Model() {
-        var _this = this;
-
-        var data = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-
-        _classCallCheck(this, Model);
-
-        wm.set(this, { initial: undefined, deleted: false });
-        var loader = function loader() {
-            wm.get(_this).initial = angular.copy(data);
-            for (var prop in data) {
-                if (prop == '$promise') {
-                    continue;
-                }
-                _this[prop] = data[prop];
-            }
-        };
-        if (data.$promise) {
-            data.$promise.then(loader);
-            Object.defineProperty(this, '$promise', { get: function get() {
-                    return data.$promise;
-                } });
-        } else {
-            loader();
-        }
-    }
-
-    _createClass(Model, [{
-        key: 'setBitflags',
-        value: function setBitflags(source) {
-            var _this2 = this;
-
-            var mapping = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
-
-            var _loop = function _loop(name) {
-                Object.defineProperty(_this2, name, {
-                    get: function get() {
-                        return !!(_this2[source] & mapping[name]);
-                    },
-                    set: function set(value) {
-                        if (!!value) {
-                            _this2[source] |= mapping[name];
-                        } else {
-                            _this2[source] &= ~mapping[name];
-                        }
-                    }
-                });
-            };
-
-            for (var name in mapping) {
-                _loop(name);
-            }
-        }
-
-        /**
-         * Virtual property to check if this model is "dirty".
-         *
-         * @return boolean True if dirty, false if pristine.
-         */
-
-    }, {
-        key: '$dirty',
-        get: function get() {
-            if (wm.get(this).deleted) {
-                return true;
-            }
-            var initial = wm.get(this).initial || {};
-            for (var prop in this) {
-                if (prop.substring(0, 1) == '$') {
-                    continue;
-                }
-                if (differs(this[prop], initial[prop])) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        /**
-         * Guesstimate the "title" of a particular model instance.
-         *
-         * @return string A guesstimated title.
-         */
-
-    }, {
-        key: '$title',
-        get: function get() {
-            // First, the usual suspects:
-            if ('title' in this) {
-                return this.title;
-            }
-            if ('name' in this) {
-                return this.name;
-            }
-            // If any field is an actual string _and_ its length is shorter
-            // than 255 chars (reasonable maximum...) use that:
-            for (var prop in this) {
-                if (prop.substring(0, 1) == '$') {
-                    continue;
-                }
-                if (typeof this[prop] == 'string' && this[prop].length && this[prop].length <= 255) {
-                    return this[prop];
-                }
-            }
-            return '[Untitled]';
-        }
-
-        /**
-         * Get private deleted state.
-         *
-         * @return bool
-         */
-
-    }, {
-        key: '$deleted',
-        get: function get() {
-            return wm.get(this).deleted;
-        }
-
-        /**
-         * Set the deleted state. Note that this does _not_ call the "$delete"
-         * resource method.
-         *
-         * @param mixed value Truthy for "scheduled for deletion".
-         */
-        ,
-        set: function set(value) {
-            wm.get(this).deleted = !!value;
-        }
-    }]);
-
-    return Model;
-}();
-
-exports.default = Model;
-;
-
-/**
- * Private helper to check if a field is actually set.
- * Unset is defined as undefined or null, string with no length OR
- * a string consisting only of <p></p> (for WYSIWTYG fields).
- *
- * @param mixed val The value to check.
- * @return bool True if the value is considered "empty", else false.
- */
-function isset(val) {
-    return !!(val !== undefined && val !== null && ('' + val).trim().length && !('' + val).trim().match(/^<p>(\s|\n|&nbsp;)*<\/p>$/));
-}
-
-/**
- * Private helper to determine if two values differ. Values can be of any type;
- * the helper guesstimates and recurses as needed.
- *
- * @param mixed a Value to check.
- * @param mixed b Value to compare to.
- * @return bool
- */
-function differs(a, b) {
-    if (!isset(a) && isset(b) || isset(a) && !isset(b)) {
-        return true;
-    }
-    if (!a && !b) {
-        return false;
-    }
-    // If `a` is "dirty" we don't even have to do any other checks.
-    if (a instanceof Model) {
-        return a.$dirty || a.$deleted;
-    }
-    if (angular.isArray(a) && angular.isArray(b)) {
-        if (a.length != b.length) {
-            return true;
-        }
-        for (var i = 0; i < a.length; i++) {
-            if (differs(a[i], b[i])) {
-                return true;
-            }
-        }
-    }
-    if (a instanceof Date && b instanceof Date) {
-        return a < b || a > b;
-    }
-    if ((typeof a === 'undefined' ? 'undefined' : _typeof(a)) == 'object' && (typeof b === 'undefined' ? 'undefined' : _typeof(b)) == 'object') {
-        for (var i in a) {
-            if (i.substring(0, 1) == '$') {
-                continue;
-            }
-            if (differs(a[i], b[i])) {
-                return true;
-            }
-        }
-        for (var i in b) {
-            if (i.substring(0, 1) == '$') {
-                continue;
-            }
-            if (!(i in a)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    return ('' + a).trim() != ('' + b).trim();
-}
-
-},{}],209:[function(require,module,exports){
-
-"use strict";
-
 require('babel-polyfill');
 
 require('angular');
@@ -50081,7 +49850,7 @@ angular.module('monad', ['monad.ng', 'monad.externals', 'monad.directives', 'mon
 // Services
 .service('moNavigation', _Navigation2.default).service('Authentication', _Authentication2.default).service('moLanguage', _Language2.default).service('moReport', _Report2.default).service('moDelete', _Delete2.default);
 
-},{"../i18n":1,"../templates":225,"./ListController":207,"./components/angular":213,"./directives/angular":215,"./factories/Resource":219,"./services/Authentication":220,"./services/Delete":221,"./services/Language":222,"./services/Navigation":223,"./services/Report":224,"angular":14,"angular-animate":3,"angular-gettext":4,"angular-resource":6,"angular-route":8,"angular-sanitize":10,"angular-ui-bootstrap":12,"autofill-event":15,"babel-polyfill":16,"ng-lollipop":205}],210:[function(require,module,exports){
+},{"../i18n":1,"../templates":224,"./ListController":207,"./components/angular":212,"./directives/angular":214,"./factories/Resource":218,"./services/Authentication":219,"./services/Delete":220,"./services/Language":221,"./services/Navigation":222,"./services/Report":223,"angular":14,"angular-animate":3,"angular-gettext":4,"angular-resource":6,"angular-route":8,"angular-sanitize":10,"angular-ui-bootstrap":12,"autofill-event":15,"babel-polyfill":16,"ng-lollipop":205}],209:[function(require,module,exports){
 
 "use strict";
 
@@ -50127,7 +49896,7 @@ angular.module('monad.components.list', []).component('moListHeader', {
     bindings: { rows: '=', update: '@' }
 });
 
-},{}],211:[function(require,module,exports){
+},{}],210:[function(require,module,exports){
 
 "use strict";
 
@@ -50140,17 +49909,11 @@ angular.module('monad.components.login', []).component('moLogin', {
     transclude: true
 });
 
-},{}],212:[function(require,module,exports){
+},{}],211:[function(require,module,exports){
 
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _Model = require('../../Model');
-
-var _Model2 = _interopRequireDefault(_Model);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -50201,16 +49964,12 @@ var controller = function () {
             };
 
             function $save(item) {
-                if (angular.isArray(item)) {
-                    item.map($save);
-                } else if (item instanceof _Model2.default) {
-                    if (item.$deleted) {
-                        operations++;
-                        item.$delete(progress);
-                    } else if (!item.id || item.$dirty) {
-                        operations++;
-                        item.$save(progress);
-                    }
+                if (item.$deleted) {
+                    operations++;
+                    item.$delete(progress);
+                } else if (item.$dirty) {
+                    operations++;
+                    item.$save(progress);
                 }
             };
 
@@ -50223,6 +49982,10 @@ var controller = function () {
         key: '$dirty',
         get: function get() {
             for (var i in this.data) {
+                if (this.data[i] == undefined) {
+                    // A resolving promise, so ignore (it'll show up on the next iteration)
+                    continue;
+                }
                 if (angular.isArray(this.data[i])) {
                     for (var j = 0; j < this.data[i].length; j++) {
                         // Deleted, dirty or new
@@ -50250,7 +50013,7 @@ angular.module('monad.components.update', []).component('moUpdate', {
     controller: controller
 });
 
-},{"../../Model":208}],213:[function(require,module,exports){
+},{}],212:[function(require,module,exports){
 
 "use strict";
 
@@ -50262,7 +50025,7 @@ require('./Update/component');
 
 angular.module('monad.components', ['monad.components.login', 'monad.components.list', 'monad.components.update']);
 
-},{"./List/component":210,"./Login/component":211,"./Update/component":212}],214:[function(require,module,exports){
+},{"./List/component":209,"./Login/component":210,"./Update/component":211}],213:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -50281,7 +50044,7 @@ exports.default = function () {
     };
 };
 
-},{}],215:[function(require,module,exports){
+},{}],214:[function(require,module,exports){
 
 "use strict";
 
@@ -50305,7 +50068,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 angular.module('monad.directives', []).directive('moField', _Field2.default).directive('moDragDrop', _dragDrop2.default).directive('moSlug', _slug2.default).directive('moMessage', _message2.default);
 
-},{"./Field":214,"./dragDrop":216,"./message":217,"./slug":218}],216:[function(require,module,exports){
+},{"./Field":213,"./dragDrop":215,"./message":216,"./slug":217}],215:[function(require,module,exports){
 
 "use strict";
 
@@ -50392,7 +50155,7 @@ function link($scope, elem, attrs) {
     });
 }
 
-},{}],217:[function(require,module,exports){
+},{}],216:[function(require,module,exports){
 
 "use strict";
 
@@ -50433,7 +50196,7 @@ exports.default = ['$compile', function ($compile) {
     };
 }];
 
-},{}],218:[function(require,module,exports){
+},{}],217:[function(require,module,exports){
 
 "use strict";
 
@@ -50487,7 +50250,7 @@ exports.default = function () {
     };
 };
 
-},{}],219:[function(require,module,exports){
+},{}],218:[function(require,module,exports){
 
 "use strict";
 
@@ -50498,6 +50261,15 @@ Object.defineProperty(exports, "__esModule", {
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 var wm = new WeakMap();
+
+function get(obj) {
+    var test = undefined;
+    if (test = wm.get(obj)) {
+        return test;
+    }
+    wm.set(obj, { initial: undefined, deleted: false });
+    return wm.get(obj);
+};
 
 /**
  * Extend default $resource service so it returns more intelligent resource
@@ -50546,17 +50318,17 @@ exports.default = ['$resource', '$rootScope', function ($resource, $rootScope) {
             }
         };
 
-        Object.defineProperty(res.prototype, '$dirty', {
+        Object.defineProperty(res, '$dirty', {
             /**
              * Virtual property to check if res model is "dirty".
              *
              * @return boolean True if dirty, false if pristine.
              */
             get: function get() {
-                if (wm.get(this).deleted) {
+                if (_get(this).deleted) {
                     return true;
                 }
-                var initial = wm.get(this).initial || {};
+                var initial = _get(this).initial || {};
                 for (var prop in this) {
                     if (prop.substring(0, 1) == '$' || typeof this[prop] == 'function') {
                         continue;
@@ -50569,7 +50341,7 @@ exports.default = ['$resource', '$rootScope', function ($resource, $rootScope) {
             }
         });
 
-        Object.defineProperty(res.prototype, '$title', {
+        Object.defineProperty(res, '$title', {
             /**
              * Guesstimate the "title" of a particular model instance.
              *
@@ -50597,14 +50369,14 @@ exports.default = ['$resource', '$rootScope', function ($resource, $rootScope) {
             }
         });
 
-        Object.defineProperty(res.prototype, '$deleted', {
+        Object.defineProperty(res, '$deleted', {
             /**
              * Get private deleted state.
              *
              * @return bool
              */
             get: function get() {
-                return wm.get(this).deleted;
+                return _get(this).deleted;
             },
             /**
              * Set the deleted state. Note that res does _not_ call the "$delete"
@@ -50613,12 +50385,12 @@ exports.default = ['$resource', '$rootScope', function ($resource, $rootScope) {
              * @param mixed value Truthy for "scheduled for deletion".
              */
             set: function set(value) {
-                wm.get(this).deleted = !!value;
+                _get(this).deleted = !!value;
             }
         });
 
         var query = res.query;
-        var get = res.get;
+        var _get = res.get;
         res.query = function (parameters, success, error) {
             var found = query.call(res, parameters, success, error);
             found.$promise.then(function () {
@@ -50666,7 +50438,7 @@ exports.default = ['$resource', '$rootScope', function ($resource, $rootScope) {
             return found;
         };
         res.get = function (parameters, success, error) {
-            var resource = get.call(res, parameters, success, error);
+            var resource = _get.call(res, parameters, success, error);
             wm.set(resource, { initial: undefined, deleted: false });
             resource.$promise.then(function () {
                 wm.get(resource).initial = copy(resource);
@@ -50763,7 +50535,7 @@ function differs(comp, a, b) {
     return ('' + a).trim() != ('' + b).trim();
 }
 
-},{}],220:[function(require,module,exports){
+},{}],219:[function(require,module,exports){
 
 "use strict";
 
@@ -50843,7 +50615,7 @@ var Authentication = function () {
 exports.default = Authentication;
 ;
 
-},{}],221:[function(require,module,exports){
+},{}],220:[function(require,module,exports){
 
 "use strict";
 
@@ -50912,7 +50684,7 @@ exports.default = Delete;
 
 Delete.$inject = ['$uibModal', 'moLanguage', '$location', '$route'];
 
-},{}],222:[function(require,module,exports){
+},{}],221:[function(require,module,exports){
 
 "use strict";
 
@@ -51007,7 +50779,7 @@ exports.default = Language;
 
 Language.$inject = ['gettextCatalog', '$rootScope'];
 
-},{}],223:[function(require,module,exports){
+},{}],222:[function(require,module,exports){
 
 "use strict";
 
@@ -51165,7 +50937,7 @@ exports.default = Navigation;
 
 Navigation.$inject = ['$location', 'Authentication'];
 
-},{}],224:[function(require,module,exports){
+},{}],223:[function(require,module,exports){
 
 "use strict";
 
@@ -51383,7 +51155,7 @@ exports.default = Report;
 
 Report.$inject = ['$timeout'];
 
-},{}],225:[function(require,module,exports){
+},{}],224:[function(require,module,exports){
 'use strict';
 
 angular.module('monad.templates', []).run(['$templateCache', function ($templateCache) {
@@ -51400,4 +51172,4 @@ angular.module('monad.templates', []).run(['$templateCache', function ($template
   $templateCache.put('/monad/templates/license.html', "<div class=modal-header>\n" + "    <h3 class=modal-title translate>License</h3>\n" + "</div>\n" + "<div class=modal-body>\n" + "    <p><strong translate>Note: this applies to the Monad CMS framework, not (necessarily) the site it is used for :)</strong></p>\n" + "    <div ng-include=\"'/monad/LICENSE.html'\"></div>\n" + "</div>\n" + "<div class=modal-footer>\n" + "    <button class=\"btn btn-primary\" ng-click=ok() translate>Got it!</button>\n" + "</div>");
 }]);
 
-},{}]},{},[209]);
+},{}]},{},[208]);
