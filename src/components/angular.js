3a91341aaacc396da8ba7f03226a2d0fc1f52c99
Marijn Ophorst <marijn@sensimedia.nl>
2016-07-07 18:09:38 +0200

    cleaner

--- src/components/angular.js
+++ src/components/angular.js
@@ -1,9 +1,9 @@
 
 "use strict";
 
-import './Login/component';
-import './List/component';
-import './Update/component';
+import login from './Login/component';
+import list from './List/component';
+import update from './Update/component';
 
-angular.module('monad.components', ['monad.components.login', 'monad.components.list', 'monad.components.update']);
+export default angular.module('monad.components', [login, list, update]).name;
 
