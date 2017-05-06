869936e030a3d04320469cb4854234c19ec4ba06
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-06 14:30:35 +0200

    load more components

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,8 @@
 import login from './Login/component';
 import list from './List/component';
 import update from './Update/component';
+import footer from './Footer';
+import tray from './Tray';
 
-export default angular.module('monad.components', [login, list, update]).name;
+export default angular.module('monad.components', ['monad.cms', login, list, update, footer, tray]).name;
 
