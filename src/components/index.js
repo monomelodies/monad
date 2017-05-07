2c3f9941a4badda10c83fc090da5f582bf853ca2
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-07 15:42:06 +0200

    capitalize for consistency

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 
 "use strict";
 
-import login from './Login/component';
-import list from './List/component';
-import update from './Update/component';
-import footer from './Footer';
-import tray from './Tray';
+import Login from './Login';
+import Footer from './Footer';
+import Tray from './Tray';
 
-export default angular.module('monad.components', ['monad.cms', login, list, update, footer, tray]).name;
+export default angular.module('monad.components', ['monad.cms', Login, Footer, Tray]).name;
 
