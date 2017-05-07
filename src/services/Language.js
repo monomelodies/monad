c2f7770957361a1da42da34d1b7ff8ba91ebade7
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-07 15:43:12 +0200

    simple fake language service

---
+++ src/services/Language.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+export default class Language {
+
+    get current() {
+        return undefined;
+    }
+
+    set current(dummy) {
+    }
+
+    get list() {
+        return undefined;
+    }
+
+};
+
