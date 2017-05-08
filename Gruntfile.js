40f7f43c3e2ce599e78ead9d2504666d4acae0cf
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-08 09:06:00 +0200

    use the correct task. also need to figure out sass naming scheme, i do not like the _sass grouping any more

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,15 +39,15 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.config('sass', {
         monad: {
             options: {
-                outputStyle: 'compressed',
+                style: 'compressed',
                 compass: true,
                 sourcemap: 'none'
             },
-            files: {'dist/monad.css': 'src/style.scss'}
+            files: {'dist/monad.css': 'src/_sass/default.scss'}
         }
     });
 
