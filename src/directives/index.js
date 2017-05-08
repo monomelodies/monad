fbebaf5c75c69adbc6429d6b751c2e37d567cae2
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-08 09:17:22 +0200

    purge directives moved to plugins, and add alias to new name

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,13 +1,10 @@
 
 "use strict";
 
-import DragDrop from './dragDrop';
-import Slug from './slug';
 import Message from './message';
 
 export default angular.module('monad.directives', ['monad.cms'])
-    .directive('moDragDrop', DragDrop)
-    .directive('moSlug', Slug)
+    .directive('monadMessage', Message)
     .directive('moMessage', Message)
     .name
     ;
