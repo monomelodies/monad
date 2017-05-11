83e1416d54f8f7721acd5ea882628d920752865f
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-11 09:06:20 +0200

    inject and store copyright

--- src/components/Footer/Controller.js
+++ src/components/Footer/Controller.js
@@ -7,11 +7,12 @@ let $uibModal = undefined;
 
 export default class controller {
 
-    constructor(_monadLanguageService_, _Authentication_, _$uibModal_, MONAD_VERSION) {
+    constructor(_monadLanguageService_, _Authentication_, _$uibModal_, MONAD_VERSION, MONAD_COPYRIGHT) {
         monadLanguageService = _monadLanguageService_;
         Authentication = _Authentication_;
         $uibModal = _$uibModal_;
         this.version = MONAD_VERSION;
+        this.copyright = MONAD_COPYRIGHT;
     }
 
     get language() {
@@ -37,5 +38,5 @@ export default class controller {
 
 };
 
-controller.$inject = ['monadLanguageService', 'Authentication', '$uibModal', 'MONAD_VERSION'];
+controller.$inject = ['monadLanguageService', 'Authentication', '$uibModal', 'MONAD_VERSION', 'MONAD_COPYRIGHT'];
 
