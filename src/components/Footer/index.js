b961470bcf340f5c72acefdb91c99c222baf250d
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-11 08:31:47 +0200

    moved to separate file for reuse/extending

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,43 +1,7 @@
 
 "use strict";
 
-let monadLanguageService = undefined;
-let Authentication = undefined;
-let $uibModal = undefined;
-
-class controller {
-
-    constructor(_monadLanguageService_, _Authentication_, _$uibModal_, MONAD_VERSION) {
-        monadLanguageService = _monadLanguageService_;
-        Authentication = _Authentication_;
-        $uibModal = _$uibModal_;
-        this.version = MONAD_VERSION;
-    }
-
-    get language() {
-        return monadLanguageService.current || 'en';
-    }
-
-    get languages() {
-        return monadLanguageService.list || ['en'];
-    }
-
-    get authenticated() {
-        return Authentication.check;
-    }
-
-    license() {
-        $uibModal.open({
-            templateUrl: 'Monad/templates/license.html',
-            controller: ['$uibModalInstance', '$scope', function ($uibModalInstance, $scope) {
-                $scope.ok = () => $uibModalInstance.dismiss();
-            }]
-        });
-    };
-
-};
-
-controller.$inject = ['monadLanguageService', 'Authentication', '$uibModal', 'MONAD_VERSION'];
+import controller from './Controller';
 
 export default angular.module('monad.components.footer', ['monad.cms'])
     .component('monadFooter', {
