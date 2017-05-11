57475ba0fbd216d091c5933c07b81ec833367410
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-11 09:08:59 +0200

    language selection moved to multilang plugin

--- src/components/Footer/Controller.js
+++ src/components/Footer/Controller.js
@@ -1,30 +1,16 @@
 
 "use strict";
 
-let monadLanguageService = undefined;
 let Authentication = undefined;
 let $uibModal = undefined;
 
 export default class controller {
 
-    constructor(_monadLanguageService_, _Authentication_, _$uibModal_, MONAD_VERSION, MONAD_COPYRIGHT) {
-        monadLanguageService = _monadLanguageService_;
+    constructor(_Authentication_, _$uibModal_, MONAD_VERSION, MONAD_COPYRIGHT) {
         Authentication = _Authentication_;
         $uibModal = _$uibModal_;
         this.version = MONAD_VERSION;
         this.copyright = MONAD_COPYRIGHT;
-    }
-
-    get language() {
-        return monadLanguageService.current || 'en';
-    }
-
-    get languages() {
-        return monadLanguageService.list || ['en'];
-    }
-
-    get authenticated() {
-        return Authentication.check;
     }
 
     license() {
@@ -38,5 +24,5 @@ export default class controller {
 
 };
 
-controller.$inject = ['monadLanguageService', 'Authentication', '$uibModal', 'MONAD_VERSION', 'MONAD_COPYRIGHT'];
+controller.$inject = ['Authentication', '$uibModal', 'MONAD_VERSION', 'MONAD_COPYRIGHT'];
 
