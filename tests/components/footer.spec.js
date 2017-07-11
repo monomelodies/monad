b73a973e8c0e006bc04a4df3524369380b3f947b
Danny van der Veldt <danny@sensimedia.nl>
2017-07-11 12:54:14 +0200

    Test footer while logged in

--- tests/components/footer.spec.js
+++ tests/components/footer.spec.js
@@ -5,6 +5,7 @@ describe('Footer component', () => {
     let element = undefined;
     let $rootScope = undefined;
     let $compile = undefined;
+    let Authentication = undefined;
 
     let mod = angular.module('tests.footer', []).service('Authentication', function () {
         this.check = false;
@@ -13,13 +14,14 @@ describe('Footer component', () => {
                 this.check = true;
             }
         };
-        this.status = () => this.check;
+        this.status = () => $rootScope.check;
     });
     beforeEach(angular.mock.module(mod.name));
 
-    beforeEach(inject((_$rootScope_, _$compile_) => {
+    beforeEach(inject((_$rootScope_, _$compile_, _Authentication_) => {
         $rootScope = _$rootScope_;
         $compile = _$compile_;
+        Authentication = _Authentication_;
     }));
 
     describe('Footer while not logged in', () => {
@@ -28,6 +30,17 @@ describe('Footer component', () => {
             element = $compile(tpl)($rootScope);
             $rootScope.$digest();
             expect(element.find('li').length).toBe(1);
+        });
+    });
+
+    describe('Footer while logged in', () => {
+        it('should display two lis', () => {
+            Authentication.check = true;
+            let tpl = angular.element('<monad-footer></monad-footer>');
+            element = $compile(tpl)($rootScope);
+            $rootScope.$digest();
+            expect(element.find('li').length).toBe(2);
+            Authentication.check = false;
         });
     });
 });
