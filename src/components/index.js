1451c16897fc0ce942d06ae5bad5ebd48b179690
Marijn Ophorst <marijn@monomelodies.nl>
2019-08-21 14:56:37 +0200

    footer moved to monad-navigation

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,7 @@
 "use strict";
 
 import Login from './Login';
-import Footer from './Footer';
 import Tray from './Tray';
 
-export default angular.module('monad.components', ['monad.cms', Login, Footer, Tray]).name;
+export default angular.module('monad.components', ['monad.cms', Login, Tray]).name;
 
